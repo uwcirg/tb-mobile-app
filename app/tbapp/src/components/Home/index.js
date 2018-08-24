95ec27eb8928c1c229bc542c2ad027e538ee2c1a
Piotr Mankowski <pmanko@uw.edu>
2018-08-24 15:15:25 -0700

    Small path fix

--- app/tbapp/src/components/Home/index.js
+++ app/tbapp/src/components/Home/index.js
@@ -16,7 +16,7 @@ const Home = () => <div className='row' id='daily-checkin'>
               <Link className='btn btn-lg' to='/daily-checkin'><i className="far fa-fw fa-calendar-check"></i>&nbsp;&nbsp;Notificación Diaria</Link>
               <Link className='btn btn-lg' to='/messages'><i className="far fa-fw fa-envelope"></i>&nbsp;&nbsp;Mensajería</Link>
               <Link className='btn btn-lg' to='/info'><i className="fas fa-fw fa-info"></i>&nbsp;&nbsp;Información y Educación</Link>
-              <a className='btn btn-lg' href={ process.env.REACT_APP_CPRO_PATH+'users/care' }><i className="fas fa-fw fa-chart-line"></i>&nbsp;&nbsp;Mi Progreso</a>
+              <a className='btn btn-lg' href={ process.env.REACT_APP_CPRO_PATH+'/users/care' }><i className="fas fa-fw fa-chart-line"></i>&nbsp;&nbsp;Mi Progreso</a>
               <Link className='btn btn-lg' to='/my-notes'><i className="fas fa-fw fa-pencil-alt"></i>&nbsp;&nbsp;Mis Notas</Link>
           </ButtonGroup>
         </div>
