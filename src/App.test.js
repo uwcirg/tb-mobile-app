7edb086e069aff11cab8c02cd9fc08aaf952c898
Grace <thecalliopecrashed@users.noreply.github.com>
2018-10-03 23:24:52 -0400

    Use `enzyme` to help with tests
    See https://airbnb.io/enzyme/

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Store from "./Store"
+import { mount } from "enzyme"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  const store = new Store();
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    const store = new Store();
 
-  ReactDOM.render(<App store={store} />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+    mount(<App store={store} />);
+  });
+})
