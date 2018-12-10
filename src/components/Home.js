8cdc8f8c249c808145a2898c80b5f72ce9d9eaf2
Grace <thecalliopecrashed@users.noreply.github.com>
2018-12-10 15:06:32 -0800

    Extract a `Calendar` component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components"
 import { observer } from "mobx-react"
-import ReactCalendar from "react-calendar/dist/entry.nostyle"
 import moment from "moment"
 
 import { lightgrey, darkgrey, grey, white } from "../colors"
@@ -13,47 +12,11 @@ import {
   mdiPill,
 } from "@mdi/js"
 
+import Calendar from "./Calendar"
+
 const Home = observer(({ store }) => (
   <Layout>
-    <Calendar
-      minDetail="year"
-      tileContent={({ date, view }) =>
-          <DateWrapper>
-            <DateNumber>{date.getDate()}</DateNumber>
-
-            { view === 'month' &&
-              store.events.find(e =>
-                e.date.unix() === moment(date).unix() &&
-                e.type === "questionnaire_response" &&
-                e.questionnaire_name === "self_report"
-              )
-                ? <Icon size="1rem" color={darkgrey} path={mdiPill} />
-                : <Icon />
-            }
-
-            { view === 'month' &&
-              store.events.find(e =>
-                e.date.unix() === moment(date).unix() &&
-                e.type === "questionnaire_response" &&
-                e.questionnaire_name === "symptoms"
-              )
-                ? <Icon size="1rem" color={darkgrey} path={mdiFormatListChecks} />
-                : <Icon />
-            }
-
-            { view === 'month' &&
-              store.events.find(e =>
-                e.date.unix() === moment(date).unix() &&
-                e.type === "observation"
-              )
-                ? <Icon size="1rem" color={darkgrey} path={mdiCamera} />
-                : <Icon />
-            }
-          </DateWrapper>
-      }
-      onClickDay={(date) => store.reportMedication(date)}
-      tileDisabled={(date) => date > new Date()}
-    />
+    <Calendar store={store} />
 
     <IconKey>
       <Icon size="2rem" color={white} path={mdiPill} />
@@ -69,47 +32,6 @@ const Home = observer(({ store }) => (
 ))
 
 const Layout = styled.div`
-`
-
-const DateWrapper = styled.div`
-  display: grid;
-  grid-column-gap: 5%;
-  grid-row-gap: 5%;
-  padding: 5%;
-  grid-template-columns: 50% 50%;
-  grid-template-rows: 50% 50%;
-`
-
-const Calendar = styled(ReactCalendar)`
-  & .react-calendar__tile {
-    background-color: ${white};
-    border: 1px solid ${lightgrey};
-    height: 4rem;
-    padding: 0;
-  }
-
-  & .react-calendar__tile time { display: none; }
-
-  & .react-calendar__tile--now {
-    background-color: ${grey};
-  }
-
-  & .react-calendar__month-view__days__day--neighboringMonth {
-    color: ${grey}
-  }
-
-  & .react-calendar__navigation button {
-    background-color: ${white};
-    border-radius: 0;
-    border: 1px solid ${grey};
-    font-size: 1rem;
-    padding: 0.5rem;
-    margin-bottom: 1rem;
-  }
-`
-
-const DateNumber = styled.span`
-  color: ${darkgrey};
 `
 
 const IconKey = styled.div`
