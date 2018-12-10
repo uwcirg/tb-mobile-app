c1b72fc32b7d803528f7439ece45651efb451b8b
Grace <thecalliopecrashed@users.noreply.github.com>
2018-12-10 15:32:33 -0800

    Add a `Yes/No` meta-question to report symptoms

--- src/components/ReportSymptoms.js
+++ src/components/ReportSymptoms.js
@@ -1,32 +1,54 @@
 import React from "react"
 import styled from "styled-components"
 import { observer } from "mobx-react"
-import { Input } from "reakit";
-import { white } from "../colors";
+import { Hidden, Input } from "reakit"
+import { white } from "../colors"
+import Button from "../primitives/Button"
 
 const ReportSymptoms = observer(({ survey }) => (
   <Layout>
     <h1>Reporta Sintomas</h1>
 
-    <Label><Checkbox /><span>Náuseas o vómitos</span></Label>
-    <Label><Checkbox /><span>Enrojectimiento de la piel, granitos, picazón</span></Label>
-    <Label><Checkbox /><span>Urticaria</span></Label>
-    <Label><Checkbox /><span>Fiebre</span></Label>
-    <Label><Checkbox /><span>Pérdida de apetito</span></Label>
-    <Label><Checkbox /><span>Visión borrosa o cambios en la forma de ver los colores</span></Label>
-    <Label><Checkbox /><span>La barriga se siente sensible o dolorido</span></Label>
-    <Label><Checkbox /><span>Coloración amarillenta en la piel o en la parte blanca de los ojos</span></Label>
-    <Label><Checkbox /><span>Dificultad para respirar</span></Label>
-    <Label><Checkbox /><span>Hinchazón de la cara, los labios, la lengua, o la garganta</span></Label>
+    <Hidden.Container>
+      { hidden =>
+        <div>
+          <p>Have you experienced any symptoms or side effects?</p>
 
-    <Label>
-      <span>Otra síntoma:</span>
-      <TextInput />
-    </Label>
+          <Choice>
+            <Button as={Hidden.Show} {...hidden} >Yes</Button>
+            <Button as={Hidden.Hide} {...hidden} >No</Button>
+          </Choice>
+
+          <Hidden {...hidden}>
+            <p>Which symptoms?</p>
+
+            <Label><Checkbox /><span>Náuseas o vómitos</span></Label>
+            <Label><Checkbox /><span>Enrojectimiento de la piel, granitos, picazón</span></Label>
+            <Label><Checkbox /><span>Urticaria</span></Label>
+            <Label><Checkbox /><span>Fiebre</span></Label>
+            <Label><Checkbox /><span>Pérdida de apetito</span></Label>
+            <Label><Checkbox /><span>Visión borrosa o cambios en la forma de ver los colores</span></Label>
+            <Label><Checkbox /><span>La barriga se siente sensible o dolorido</span></Label>
+            <Label><Checkbox /><span>Coloración amarillenta en la piel o en la parte blanca de los ojos</span></Label>
+            <Label><Checkbox /><span>Dificultad para respirar</span></Label>
+            <Label><Checkbox /><span>Hinchazón de la cara, los labios, la lengua, o la garganta</span></Label>
+
+            <Label>
+              <span>Otra síntoma:</span>
+              <TextInput />
+            </Label>
+          </Hidden>
+        </div>
+      }
+    </Hidden.Container>
   </Layout>
 ))
 
 const Layout = styled.div`
+`
+
+const Choice = styled.div`
+  margin-bottom: 1rem;
 `
 
 const Label = styled.label`
