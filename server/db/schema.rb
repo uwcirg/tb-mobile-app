6a5fd685c732387001720a8b18c935b7210f9340
Grace <thecalliopecrashed@users.noreply.github.com>
2018-12-12 17:28:23 -0500

    Add a user model

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,6 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181212221603) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", primary_key: "uuid", id: :string, force: :cascade do |t|
+    t.string "name"
+    t.date "treatment_start"
+    t.string "timestamps"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["uuid"], name: "index_users_on_uuid"
+  end
 
 end
