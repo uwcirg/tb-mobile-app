d56fbb541a2f9bc9f37ed4e42b8554fa61f24be9
Grace <thecalliopecrashed@users.noreply.github.com>
2019-01-30 16:59:55 -0800

    Simplify calendar; only display medication dates

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -6,7 +6,6 @@ import moment from "moment"
 
 import { darkgrey, grey, white, green } from "../colors"
 
-import { Icon } from "@mdi/react"
 import {
   mdiCamera,
   mdiFormatListChecks,
@@ -18,66 +17,15 @@ const Calendar = observer(({ store }) => (
     minDetail="year"
     tileContent={
       ({ date, view }) => (
-        <DateWrapper
+        <DateCell
           date={date}
-          medication_report={view === 'month' && store.medication_reports.find(e => e.date.unix() === moment(date).unix())}
-          symptom_report={view === 'month' && store.symptom_reports.find(e => e.date.unix() === moment(date).unix())}
-          strip_report={view === 'month' && store.strip_reports.find(e => e.date.unix() === moment(date).unix())}
+          medication_report={view === 'month' && date < new Date() }
         >
-          <DateNumber>{date.getDate()}</DateNumber>
-          <PillIcon />
-          <SymptomIcon />
-          <PhotoIcon />
-        </DateWrapper>
+          {date.getDate()}
+        </DateCell>
     )}
   />
 ))
-
-const PillIcon = styled(Icon).attrs({
-  size: "0.5rem",
-  color: darkgrey,
-  path: mdiPill,
-})``
-
-const SymptomIcon = styled(Icon).attrs({
-  size: "0.5rem",
-  color: darkgrey,
-  path: mdiFormatListChecks,
-})``
-
-const PhotoIcon = styled(Icon).attrs({
-  size: "0.5rem",
-  color: darkgrey,
-  path: mdiCamera,
-})``
-
-const DateWrapper = styled.div`
-  background:       ${(p) => p.date < new Date() ? green : "none" };
-  border: 2px solid ${(p) => p.date < new Date() ? white : darkgrey };
-  color:            ${(p) => p.date < new Date() ? white : darkgrey };
-
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
-  border-radius: 50%;
-  margin-bottom: 0.5rem;
-  margin-right: 0.5rem;
-  padding: 10%;
-  height: 2rem;
-  width: 2rem;
-
-  ${PillIcon} {
-    ${p => p.medication_report || "display: none"}
-  }
-
-  ${SymptomIcon} {
-    ${p => p.symptom_report || "display: none"}
-  }
-
-  ${PhotoIcon} {
-    ${p => p.strip_report || "display: none"}
-  }
-`
 
 const CalendarComponent = styled(ReactCalendar)`
   width: 100% !important;
@@ -97,12 +45,24 @@ const CalendarComponent = styled(ReactCalendar)`
     border-radius: 0;
     text-decoration: underline;
     font-size: 1rem;
-    padding: 0.5rem;
   }
 `
 
-const DateNumber = styled.span`
+const DateCell = styled.div`
+  background:       ${(p) => p.date < new Date() ? green : "none" };
+  ${p => p.date < new Date() && `border: 2px solid ${white}`}
+  color:            ${(p) => p.date < new Date() ? white : darkgrey };
+
+  border-radius: 50%;
+  margin-bottom: 0.5rem;
+  margin-right: 0.5rem;
+  height: 2.5rem;
+  width: 2.5rem;
+
+  font-size: 1rem;
   font-weight: 700;
+  display: block;
+  padding-top: 0.5rem;
 `
 
 export default Calendar;
