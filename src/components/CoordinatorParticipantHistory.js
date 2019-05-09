8d41154c89b916aa418c48d83980fb7b56aa40a7
Sean Campbell <seanpc@uw.edu>
2019-05-09 13:19:07 -0700

    Data export working on medication and symptom reports, a couple bugs

--- src/components/CoordinatorParticipantHistory.js
+++ src/components/CoordinatorParticipantHistory.js
@@ -6,7 +6,7 @@ import { DateTime } from "luxon"
 
 import { darkgrey, white } from "../colors"
 
-import { Table } from "reakit";
+import { Table, Button } from "reakit";
 import Heading from "../primitives/Heading"
 import PhotoPopout from "../primitives/PhotoPopout"
 import InternalLink from "../primitives/InternalLink"
@@ -58,11 +58,64 @@ const CoordinatorParticipantHistory = observer(({ assembly }) => (
             .toLocaleString(DateTime.DATETIME_SHORT)
           }
         </Information>
+
         <Label>
           {assembly.translate("progress.days")}
         </Label>
         <Information>
           {days_of_treatment(assembly.participant_history.information)}
+        </Information>
+
+        <Label>
+          Download All Patient Data
+        </Label>
+        <Information>
+          <Button onClick={(e) => {
+                        console.log("CLICKED", assembly.participant_history.information)
+                        exportAllDataToCSV(assembly.participant_history.information)
+                        e.stopPropagation()
+                      }}>
+            Here
+          </Button>
+        </Information>
+
+        <Label>
+          Download Medication Data
+        </Label>
+        <Information>
+          <Button onClick={(e) => {
+                        console.log("CLICKED", assembly.participant_history)
+                        exportMedicationDataToCSV(assembly.participant_history.information.medication_reports)
+                        e.stopPropagation()
+                      }}>
+            Here
+          </Button>
+        </Information>
+
+        <Label>
+          Download Symptom Data
+        </Label>
+        <Information>
+          <Button onClick={(e) => {
+                        console.log("CLICKED", assembly.participant_history)
+                        exportSideEffectDataToCSV(assembly.participant_history.information.symptom_reports)
+                        e.stopPropagation()
+                      }}>
+            Here
+          </Button>
+        </Information>
+
+        <Label>
+          Download Strip Report Data
+        </Label>
+        <Information>
+          <Button onClick={(e) => {
+                        console.log("CLICKED", assembly.participant_history)
+                        exportStripReportDataToCSV(assembly.participant_history.information.strip_reports)
+                        e.stopPropagation()
+                      }}>
+            Here
+          </Button>
         </Information>
       </Info>
 
@@ -185,6 +238,111 @@ const CoordinatorParticipantHistory = observer(({ assembly }) => (
     </Split>
   </Layout>
 ))
+
+// Help from: https://www.codexworld.com/export-html-table-data-to-csv-using-javascript/
+function exportAllDataToCSV(participant_information) {
+  let all_data = [];
+  // get the medication reports
+  let medication_csv = exportMedicationDataToCSV(participant_information.medication_reports)
+
+  // get the symptom_reports
+  let symptom_csv = exportSideEffectDataToCSV(participant_information.symptom_reports);
+  
+  // TODO: Strip report information
+  // strip_reports = participant_information.strip_reports;
+
+  // TODO: Resolution information
+  
+  // need to think about out of bounds errors in the future
+  for (let i = 0; i < medication_csv.length; i++) {
+    all_data[i] = medication_csv[i] + "," + symptom_csv[i];
+  }
+
+  // Download CSV file
+  downloadCSV(all_data.join("\n"), "all_patient_data.csv");
+}
+
+
+// Returns a CSV with medication data to a CSV
+function exportMedicationDataToCSV(medication_reports) {
+  var csv = [];
+  csv.push("Timestamp_Medication,Took_Medication,Not_Taking_Reason");
+  
+  for (var i = 0; i < medication_reports.length; i++) {
+    var row = medication_reports[i];
+    csv.push(row.timestamp + "," + row.took_medication + "," + row.not_taking_medication_reason);
+  }
+  return csv;
+  // debugger
+
+
+  // Download CSV file
+  // downloadCSV(csv.join("\n"), "medication_data.csv");
+}
+
+// Returns a CSV with side effect data to a CSV
+// TODO: see if it works with multiple side effects, does patient history work with multiple side effects?
+// 27: "2019-05-09T12:41:00.000Z,fever,appetite_loss,0,Other symptoms too" for a multiple report, let's see how it looks
+function exportSideEffectDataToCSV(symptom_reports) {
+  var csv = [];
+  csv.push("Timestamp_Side_Effect,Reported_Symptoms,Nausea_Rating,Other_Symptoms");
+  
+  for (var i = 0; i < symptom_reports.length; i++) {
+    var row = symptom_reports[i];
+    // for the case when they have multiple symptoms to report
+    // this is not an elegant solution, we should consider seperating these differently
+    var reported_symptoms = row.reported_symptoms.join(" ");
+    csv.push(row.timestamp + "," + reported_symptoms + "," + row.nausea_rating + "," + row.other);
+  }
+  return csv;
+  // debugger
+
+  // Download CSV file
+  // downloadCSV(csv.join("\n"), "side_effect_data.csv");
+}
+
+// Returns a CSV with side effect data to a CSV
+function exportStripReportDataToCSV(strip_reports) {
+  var csv = [];
+  csv.push("Created_At_Strip_Report,Status,Photo");
+  
+  for (var i = 0; i < strip_reports.length; i++) {
+    var row = strip_reports[i];
+    csv.push(row.created_at + "," + row.status + ",Photo_Here");
+  }
+
+  // Download CSV file
+  downloadCSV(csv.join("\n"), "strip_report_data.csv");
+}
+
+
+
+// This function is working just fine
+function downloadCSV(csv, filename) {
+  var csvFile;
+  var downloadLink;
+
+  // CSV file
+  csvFile = new Blob([csv], {type: "text/csv"});
+
+  // Download link
+  downloadLink = document.createElement("a");
+
+  // File name
+  downloadLink.download = filename;
+
+  // Create a link to the file
+  downloadLink.href = window.URL.createObjectURL(csvFile);
+
+  // Hide download link
+  downloadLink.style.display = "none";
+
+  // Add the link to DOM
+  document.body.appendChild(downloadLink);
+
+  // Click download link
+  downloadLink.click();
+}
 
 const Layout = styled.div`
   background-color: ${white};
