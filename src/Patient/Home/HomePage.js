d427171e4473db87202e3a5e9c71df10fac2498a
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2020-05-18 18:54:30 -0400

    Testing CD Integration

--- src/Patient/Home/HomePage.js
+++ src/Patient/Home/HomePage.js
@@ -15,7 +15,7 @@ const HomePage = () => {
             <ActionBox />
             <Progress />
             <Alerts />
-            <p>Version 0.9.1</p>
+            <p>Version 0.9.5-18-6:50</p>
         </Body>
     )
 
