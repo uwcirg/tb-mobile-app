2c3ab9da392219133a02a37f463196b220efc2f5
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2020-07-30 16:03:33 -0400

    Photo submission statuses added to report card

--- src/Practitioner/PatientProfile/ReportList.js
+++ src/Practitioner/PatientProfile/ReportList.js
@@ -12,6 +12,10 @@ import ExpandButton from '@material-ui/icons/KeyboardArrowDown'
 import CollapseButton from '@material-ui/icons/KeyboardArrowUp'
 import Collapse from '@material-ui/core/Collapse';
 
+import Check from '@material-ui/icons/Check'
+import Clear from '@material-ui/icons/Clear'
+import Pending from '@material-ui/icons/Help';
+
 
 
 const useStyles = makeStyles({
@@ -20,7 +24,7 @@ const useStyles = makeStyles({
         maxHeight: "78vh",
         overflow: "scroll"
     },
-    report:{
+    report: {
         ...Styles.profileCard,
         boxSizing: "border-box",
         width: "95%",
@@ -34,15 +38,15 @@ const useStyles = makeStyles({
         display: "flex",
         padding: ".5em 1em .5em 1em",
         alignItems: "center",
-        "& > div.section":{
+        "& > div.section": {
             marginLeft: "1em",
             paddingRight: ".5em",
             borderRight: "solid 1px gray"
         },
-        "& > div.section:last-of-type":{
+        "& > div.section:last-of-type": {
             borderRight: "none"
         },
-        "& > button.expand":{
+        "& > button.expand": {
             marginLeft: "auto"
         }
     },
@@ -75,32 +79,36 @@ const useStyles = makeStyles({
         fontSize: ".875em",
         letterSpacing: ".15px",
         color: Colors.textDarkGray,
-        "& > span":{
+        "& > span": {
             fontWeight: "bold",
             textTransform: "capitalize"
         },
-        "& > span, & > p":{
+        "& > span, & > p": {
             margin: 0,
             padding: 0
         },
-        "& > p":{
+        "& > p": {
             marginBottom: "3px"
         },
     },
-    details:{
+    details: {
         display: "flex"
     },
-    reportPhoto:{
+    reportPhoto: {
         flexBasis: "25%",
         marginLeft: "auto",
-        "& > img":{
+        "& > img": {
             width: "100%"
         }
     },
-    noPhoto:{
+    noPhoto: {
         ...Styles.flexCenter,
         height: "100px",
         backgroundColor: Colors.lightgray
+    },
+    photoStatus:{
+        display: "flex",
+        alignItems: "center"
     }
 })
 
@@ -118,7 +126,7 @@ const ReportView = observer(() => {
 })
 
 const Report = (props) => {
-    const [expanded,setExpanded] = useState(false);
+    const [expanded, setExpanded] = useState(false);
     const { report } = props;
     const classes = useStyles();
     const date = DateTime.fromISO(report.date);
@@ -127,43 +135,58 @@ const Report = (props) => {
     console.log(report)
 
     return (
-        
+
         <div className={classes.report}>
-        <div className={classes.preview}>
-            <div className={classes.time}>
-                <span>{date.day}</span>
-                <p>{date.monthShort}</p>
+            <div className={classes.preview}>
+                <div className={classes.time}>
+                    <span>{date.day}</span>
+                    <p>{date.monthShort}</p>
+                </div>
+                <Tag backgroundColor={Colors.patientHistory.report}>Report</Tag>
+                <ReportItem title={t('report.medicationTaken')} content={report.medicationWasTaken ? t('commonWords.yes') : t('commonWords.no')} />
+                <ReportItem title={t('report.time')} content={DateTime.fromISO(report.takenAt).toLocaleString(DateTime.TIME_24_SIMPLE)} />
+                <ReportItem title={t('commonWords.symptoms')} content={<SymptomList list={report.symptoms} />} />
+                <IconButton className="expand" onClick={() => { setExpanded(!expanded) }}>{expanded ? <CollapseButton /> : <ExpandButton />}</IconButton>
             </div>
-            <Tag backgroundColor={Colors.patientHistory.report}>Report</Tag>
-            <ReportItem title={t('report.medicationTaken')} content={report.medicationWasTaken ? t('commonWords.yes') : t('commonWords.no')} />
-            <ReportItem title={t('report.time')} content={DateTime.fromISO(report.takenAt).toLocaleString(DateTime.TIME_24_SIMPLE)} />
-            <ReportItem title={t('commonWords.symptoms')} content={<SymptomList list={report.symptoms} />} />
-            <IconButton className="expand" onClick={() => {setExpanded(!expanded)}}>{expanded ?<CollapseButton /> : <ExpandButton />}</IconButton>
+            <Collapse in={expanded}>
+                <div className={classes.details}>
+                    <ReportItem title={t('report.medicationTaken')} content={report.medicationWasTaken ? t('commonWords.yes') : t('commonWords.no')} />
+                    <ReportItem title={t('report.time')} content={DateTime.fromISO(report.takenAt).toLocaleString(DateTime.TIME_24_SIMPLE)} />
+                    <ReportItem title={t('commonWords.symptoms')} content={<SymptomList list={report.symptoms} />} />
+                    <ReportPhoto approval={report.photoDetails && report.photoDetails.approvalStatus} url={report.photoUrl} />
+                </div>
+            </Collapse>
         </div>
-        <Collapse in={expanded}>
-            <div className={classes.details}>
-            <ReportItem title={t('report.medicationTaken')} content={report.medicationWasTaken ? t('commonWords.yes') : t('commonWords.no')} />
-            <ReportItem title={t('report.time')} content={DateTime.fromISO(report.takenAt).toLocaleString(DateTime.TIME_24_SIMPLE)} />
-            <ReportItem title={t('commonWords.symptoms')} content={<SymptomList list={report.symptoms} />} />
-            <ReportPhoto url={report.photoUrl} />
-            </div>
-         </Collapse>
-        </div>
-        
+
     )
 }
 
-const ReportPhoto = (props) =>{
+const ReportPhoto = (props) => {
+    const { t } = useTranslation('translation');
     const classes = useStyles();
     return (<div className={classes.reportPhoto}>
-        {props.url ? <img src={props.url} /> : <div className={classes.noPhoto}>Photo Not Required</div>}
+        {props.url ? <><img src={props.url} />
+            <div className={classes.photoStatus}>{props.approval === null ? <>
+            <Pending style={{ color: Colors.yellow }} />
+            {t('report.pending')} </> : (props.approval === true ?
+
+                <>
+                    <Check style={{ color: Colors.green }} />
+                    {t('report.conclusive')}
+                </>
+                :
+                <>
+                    <Clear style={{ color: Colors.red }} />
+                    {t('report.inconclusive')} </>)}</div>
+                </>
+            : <div className={classes.noPhoto}>{t('report.photoNotNeeded')}</div>}
     </div>)
 }
 
 const SymptomList = (props) => {
     const { t, i18n } = useTranslation('translation');
     return (<>
-        {props.list.length > 0 ? t(`symptoms.${props.list[0]}.title`) : t('coordinator.recentReports.none')}  
+        {props.list.length > 0 ? t(`symptoms.${props.list[0]}.title`) : t('coordinator.recentReports.none')}
         {props.list.length > 1 && ` +${props.list.length - 1}`}
     </>)
 }
