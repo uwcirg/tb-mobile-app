027ad9798a8b906e8190f555cdae44fef161d65a
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2020-07-31 11:19:03 -0400

    Symptom report sorting working

---
+++ src/Basics/SymptomsSeperation.js
@@ -0,0 +1,18 @@
+const Symptoms = [
+    "sore_belly",
+    "nausea",
+    "redness",
+    "hives",
+    "fever",
+    "appetite_loss"
+  
+  ]
+  
+  const SevereSymptoms = [
+    "blurred_vision",
+    "yellow_coloration",
+    "difficulty_breathing",
+    "facial_swelling"
+  ]
+
+export {Symptoms,SevereSymptoms}
--- src/Practitioner/PatientProfile/SymptomSummary.js
+++ src/Practitioner/PatientProfile/SymptomSummary.js
@@ -1,19 +1,70 @@
-import React,{useState} from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react'
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import useStores from '../../Basics/UseStores';
-import {observer} from 'mobx-react'
+import { observer } from 'mobx-react'
 import Typography from '@material-ui/core/Typography'
 import { useTranslation } from 'react-i18next';
 import Styles from '../../Basics/Styles';
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Colors from '../../Basics/Colors';
+import { SevereSymptoms } from '../../Basics/SymptomsSeperation';
 
+const BorderLinearProgress = withStyles((theme) => ({
+    root: {
+        height: 16,
+        borderRadius: 10.5,
+    },
+    colorPrimary: {
+        backgroundColor: Colors.lightgray,
+    },
+    bar: {
+        borderRadius: 10.5,
+        backgroundColor: props => props.severe ? '#EB5757' : '#F2C94C',
+    },
+}))(LinearProgress);
 
 const useStyles = makeStyles({
-  container:{
-      padding: "1em",
-      flexBasis: "33%",
-      flexGrow: 1,
-      backgroundColor: "white",
-  }
+    container: {
+        padding: "1em 1.5em 1.5em 1.5em",
+        flexBasis: "33%",
+        flexGrow: 1,
+        backgroundColor: "white",
+    },
+    menuItem: {
+        textTransform: "capitalize"
+    },
+    top: {
+        display: "flex",
+        alignItems: "center",
+        "& > h2": {
+            margin: "0 auto 0 0",
+            ...Styles.patientPageTitle
+        }
+    },
+    barLabel: {
+        display: "flex",
+        "& > p:first-of-type": {
+            marginRight: "auto"
+        },
+        "& > p:nth-of-type(2)": {
+            fontWeight: "bold"
+        },
+
+        "& > p": {
+            color: Colors.textDarkGray,
+            fontSize: ".875em",
+            marginBottom: ".25em"
+        }
+    },
+    barContainer: {
+        width: "80%",
+    },
+    select:{
+        fontSize: ".875em",
+        "& > div": {padding: ".5em"}
+    }
 })
 
 const SymptomSummary = observer(() => {
@@ -21,19 +72,46 @@ const SymptomSummary = observer(() => {
     const classes = useStyles();
     const symptomSummary = useStores().practitionerStore.selectedPatient.symptomSummary
     const { t, i18n } = useTranslation('translation');
-    const [selection,setSelection] = useState("week");
+    const [selection, setSelection] = useState("week");
 
-    const options = ["week","month","all"]
+    const options = ["week", "month", "all"]
 
-    return(
-    <div className={classes.container}>
-        <Typography variant={"h2"}>Symptoms</Typography>
+    const handleChange = (event) => {
+        setSelection(event.target.value);
+    }
 
-       {Object.keys(symptomSummary).length > 0 && Object.keys(symptomSummary[selection]).map(each => {
-           return (symptomSummary[selection][each] > 0 ? <p>{t(`symptoms.${each}.title`)}: {symptomSummary[selection][each]} </p> : "")
-       })}
+    const list = Object.keys(symptomSummary).length > 0 ? Object.keys(symptomSummary[selection]) : []
+    const total = list.length > 0 ? Object.values(symptomSummary[selection]).reduce((acc, cur) => { return acc + cur }) : 0
 
-    </div>)
+    return (
+        <div className={classes.container}>
+            <Typography variant={"h2"}>Symptoms</Typography>
+
+            <Select
+                className={classes.select}
+                variant="outlined"
+                labelId="interval-select-label"
+                id="interval-select"
+                value={selection}
+                onChange={handleChange}
+            >
+                {options.map(each => {
+                    return <MenuItem value={each}><span className={classes.menuItem}>{t(`coordinator.patientProfile.symptomSummary.timeSelect.${each}`)}</span></MenuItem>
+                })}
+
+            </Select>
+
+            <div className={classes.barContainer}>
+                {list.map(each => {
+                    const value = symptomSummary[selection][each]
+                    return (symptomSummary[selection][each] > 0 ? <>
+                        <div className={classes.barLabel}><p>{t(`symptoms.${each}.title`)}</p> <p>{value} | {total}</p></div>
+                        <BorderLinearProgress severe={SevereSymptoms.includes(each)} variant="determinate" value={(value / total) * 100} />
+                    </> : "")
+                })}
+            </div>
+
+        </div>)
 
 });
 
