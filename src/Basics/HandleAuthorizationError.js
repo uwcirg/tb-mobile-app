7246e04a89a76e1e5d44b5fc050374f57ad88c0f
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2020-10-12 16:45:33 -0400

    Small fixes from testing

--- src/Basics/HandleAuthorizationError.js
+++ src/Basics/HandleAuthorizationError.js
@@ -2,11 +2,13 @@ import React from 'react'
 import {observer} from 'mobx-react'
 import useStores from './UseStores'
 import Alert from './Alert'
+import { useTranslation } from 'react-i18next';
 
 const CheckAuthorization = observer(() => {
 
     const {uiStore, routingStore, loginStore} = useStores();
     const { push } = routingStore;
+    const {t} = useTranslation('translation');
     
     const close = () => {
         uiStore.resetAuthError();
@@ -19,7 +21,11 @@ const CheckAuthorization = observer(() => {
 
     return (<>
    
-    {uiStore.authError && <Alert onClose={close} text={"Your session has expired and you need to log back in"}></Alert>}
+    {uiStore.authError && <Alert 
+    severity="warning"
+    position="bottom"
+    onClose={close}
+    text={t('errors.sessionExpired')} />}
     </>
     )
 
