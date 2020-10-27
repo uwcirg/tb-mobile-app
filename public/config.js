70225846febc35114ee9e95eb7e335071a2df846
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2020-10-27 15:19:33 -0400

    Disable data seeding on production

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 //Environment variables for development, when not using docker
 //These will be overriden when a build artifact is created (see Dockerfile)
 console.log("Using Development Environment Variables. If needed configure /public/config.js")
-const react_env = {URL_API: "http://localhost:5061",URL_CLIENT:"http://localhost:3000", DEFAULT_LOCALE: "es-AR", ENVIRONMENT: "development"}
+const react_env = {URL_API: "http://localhost:5061",URL_CLIENT:"http://localhost:3000", DEFAULT_LOCALE: "es-AR", ENVIRONMENT: "development", DOCKER_TAG: "develop"}
