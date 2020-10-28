2b0e0f3c48eb64345b626e093937e05ea79ff38a
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2020-10-28 13:51:48 -0400

    Added matomo config

---
+++ src/AnalyticsConfig.js
@@ -0,0 +1,28 @@
+import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
+import React from 'react'
+
+const isProduction = window && window._env && window._env.MATOMO_URL
+
+const instance = isProduction ? createInstance({
+    urlBase: isProduction ? window._env.MATOMO_URL : "",
+    siteId: isProduction ? window._env.MATOMO_ID : "",
+    userId: isProduction ? window._env.MATOMO_ID : ""
+}) : {};
+
+const MatomoConfig = (props) => {
+
+    if (isProduction) {
+        return (
+            <MatomoProvider value={instance}>
+                {props.children}
+            </MatomoProvider>
+        )
+    }
+
+    return (<>
+        {props.children}
+    </>)
+
+}
+
+export default MatomoConfig;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,47 @@
-import React, {Suspense} from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import Main from './Main';
 import { Provider } from 'mobx-react';
-import {reaction} from 'mobx';
+import { reaction } from 'mobx';
 import './App.css';
 
 //Internationalization
 import i18n from "./Language/i18n";
-import { I18nextProvider} from 'react-i18next';
+import { I18nextProvider } from 'react-i18next';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers/MuiPickersUtilsProvider';
 import DateFnsUtils from '@date-io/luxon';
 
 //Object Containing MobX Stores in ./Datastores file
-import  {stores,history} from './DataStores'
+import { stores, history } from './DataStores'
 import { Router } from 'react-router';
 import { Settings } from 'luxon';
+import MatomoConfig from './AnalyticsConfig';
 
 //Sync Language + Time Locale Changes 
 reaction(
-    () => stores.uiStore.locale,
-    locale => {
-      console.log(locale)
-      i18n.changeLanguage(locale);
-      Settings.defaultLocale = locale;
-    }
-  );
+  () => stores.uiStore.locale,
+  locale => {
+    console.log(locale)
+    i18n.changeLanguage(locale);
+    Settings.defaultLocale = locale;
+  }
+);
 
 ReactDOM.render(
-    <Provider {...stores}>
-        <I18nextProvider i18n={i18n}>
-            <Suspense fallback="Page Loading">
-            <MuiPickersUtilsProvider utils={DateFnsUtils} >
+  <Provider {...stores}>
+    <I18nextProvider i18n={i18n}>
+      <Suspense fallback="Page Loading">
+        <MatomoConfig>
+          <MuiPickersUtilsProvider utils={DateFnsUtils} >
             <Router history={history}>
               <Main />
-              </Router>
-              </MuiPickersUtilsProvider>
-            </Suspense>
-        </I18nextProvider>
-      </Provider>
-        , document.getElementById('root'));
+            </Router>
+          </MuiPickersUtilsProvider>
+        </MatomoConfig>
+      </Suspense>
+    </I18nextProvider>
+  </Provider>
+  , document.getElementById('root'));
 
 serviceWorker.register();
