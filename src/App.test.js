adbf14d64061fa33864b302e33c87b0e2cc0249e
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2020-11-03 09:02:06 -0500

    Update datastores for tests

--- src/App.test.js
+++ src/App.test.js
@@ -11,6 +11,8 @@ import { I18nextProvider} from 'react-i18next';
 import { PatientStore } from "./DataStores/patientStore";
 import {UIStore} from "./DataStores/uiStore"
 
+import {stores} from './DataStores'
+
 import APIHelper from './DataStores/Requests'
 //This attaches the class containing the API fetch requests to the stores
 //Doing it this way allows you to swap in other data retrival methods for testing
@@ -20,10 +22,10 @@ import APIHelper from './DataStores/Requests'
 it('renders without crashing', () => {
   const apiHelper = new APIHelper();
   const div = document.createElement('div');
-  const stores = {
-    uiStore: new UIStore(),
-    patientStore: new PatientStore(apiHelper)
-}
+//   const stores = {
+//     uiStore: new UIStore(),
+//     patientStore: new PatientStore(apiHelper)
+// }
 
   ReactDOM.render(<Provider {...stores}>
     <I18nextProvider i18n={i18n}>
