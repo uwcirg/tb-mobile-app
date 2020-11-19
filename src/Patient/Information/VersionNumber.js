2dc5260df54c01c1658d39f239c7463db2acf758
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2020-11-19 12:03:39 -0500

    Add link to github via version number

--- src/Patient/Information/VersionNumber.js
+++ src/Patient/Information/VersionNumber.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from 'react-i18next';
+import { Typography } from '@material-ui/core';
+import Colors from '../../Basics/Colors'
 
 const useStyles = makeStyles({
     container:{
+        display: "flex",
+        paddingTop: "1em",
         width:"100%",
-        textAlign: "center"
+        textAlign: "center",
+        alignItems:"center",
+        justifyContent:"center",
+        "& > a":{
+            color: Colors.buttonBlue,
+            marginLeft: "5px",
+            textDecoration: "none"
+        }
     }
 })
 
@@ -14,9 +25,9 @@ const VersionNumber = () => {
     const classes = useStyles();
     const { t, i18n } = useTranslation('translation');
 
-    return (<p className={classes.container}>
-        Version {process.env.REACT_APP_GITHUB_VERSION || "Unknown"}
-    </p>)
+    return (<div className={classes.container}>
+        <Typography variant="body1">{t('patient.information.version')}: </Typography> <a href={`https://github.com/uwcirg/tb-mobile-app/commit/${process.env.REACT_APP_BUILD_NUMBER}`}>{process.env.REACT_APP_GITHUB_VERSION || "Unknown"}</a>
+    </div>)
 
 }
 
