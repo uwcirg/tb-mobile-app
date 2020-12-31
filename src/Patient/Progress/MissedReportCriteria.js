f9a8c9515e5cd4b685efe709083f10b67c689516
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2020-12-31 12:55:14 -0800

    Fix styles for larger spanish text of missed days text

--- src/Patient/Progress/MissedReportCriteria.js
+++ src/Patient/Progress/MissedReportCriteria.js
@@ -5,6 +5,8 @@ import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
 import ClickableText from '../../Basics/ClickableText'
 import Collapse from '@material-ui/core/Collapse'
 import { useTranslation } from 'react-i18next';
+import { ButtonBase } from '@material-ui/core'
+import Colors from '../../Basics/Colors'
 
 const useStyles = makeStyles({
   uncapitalize: {
@@ -12,8 +14,18 @@ const useStyles = makeStyles({
   },
   noReport: {
     "& > p": {
-      margin: 0,
-      marginTop: "5px"
+      margin: "5px 0 .5em 0"
+    }
+  },
+  description:{
+    color: Colors.buttonBlue,
+    fontSize: "1em",
+    display: "flex",
+    "& > span": {
+      textAlign: "left"
+    },
+    "& > svg":{
+      marginLeft: 'auto'
     }
   }
 })
@@ -29,7 +41,11 @@ const MissedReportInfo = (props) => {
   return (
     <div className={`${classes.noReport} ${props.className}`}>
       {!props.hideReport && <p>{t('patient.progress.noReport')}</p>}
-      <ClickableText className={classes.uncapitalize} hideIcon onClick={toggleDetail} text={<>{t('patient.progress.whenBackReport')} {showDetail ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}</>}></ClickableText>
+      {/*<ClickableText className={classes.uncapitalize} hideIcon onClick={toggleDetail} text={<>{t('patient.progress.whenBackReport')} {showDetail ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}</>}></ClickableText>*/}
+      <ButtonBase onClick={toggleDetail} className={classes.description}>
+        <span>{t('patient.progress.whenBackReport')}</span>
+        {showDetail ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+      </ButtonBase>
       <Collapse in={showDetail}>
         <p>{t('patient.progress.submissionLimit')}</p>
       </Collapse>
