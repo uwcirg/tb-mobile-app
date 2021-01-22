304e1219d1b66b8549fd554cf54ada7114504b70
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-01-22 16:02:14 -0500

    Fix edit button icon and positioning on report confirmation

--- src/Basics/PatientReport.js
+++ src/Basics/PatientReport.js
@@ -9,9 +9,10 @@ import PillIcon from './Icons/Pill.js'
 import { DateTime } from 'luxon';
 import { useTranslation } from 'react-i18next';
 import useStores from '../Basics/UseStores'
-import EditIcon from '@material-ui/icons/Edit'
+import EditIcon from '@material-ui/icons/ExitToApp'
 import TempIcon from './Icons/Temp'
 import Button from '@material-ui/core/Button'
+import FaceIcon from '@material-ui/icons/Face';
 
 const useStyles = makeStyles({
     container: {
@@ -74,18 +75,23 @@ const useStyles = makeStyles({
         color: Colors.calendarRed
     },
     edit: {
+        alignItems: "flex-end",
         textTransform: "capitalize",
         color: Colors.buttonBlue,
         marginLeft: "auto",
-        "& > span > svg": {
-            fontSize: "1em",
+        "& > span":{
+            justifyContent: "flex-end",
+            flexDirection: "column"
         },
-        "& > span > span":{
-            marginLeft: "5px"
+        "& > span > svg": {
+            fontSize: "1.5em",
+        },
+        "& > span > span": {
+            marginRight: "5px"
         }
     },
-    symptoms:{
-        "& > p":{
+    symptoms: {
+        "& > p": {
             margin: "0em 0 .5em 0"
         }
     }
@@ -98,13 +104,16 @@ const PatientReport = (props) => {
     const { t } = useTranslation('translation');
 
     return (<div className={`${classes.container}`}>
-        <ListItem negative={!props.medicationWasTaken} icon={<PillIcon />} title={t("commonWords.medication")} editAction={patientUIStore.editReport} hideEdit={props.pastReport} >
+        <ListItem first negative={!props.medicationWasTaken} icon={<PillIcon />} title={t("commonWords.medication")} editAction={patientUIStore.editReport} hideEdit={props.pastReport} >
             <p> {props.medicationWasTaken ? `${t("patient.report.confirmation.takenAt")} ${DateTime.fromISO(props.timeTaken).toLocaleString(DateTime.TIME_24_SIMPLE)}` : `${t("patient.report.confirmation.notTaken")}:`}</p>
             {props.medicationNotTakenReason && <p>{props.medicationNotTakenReason}</p>}
         </ListItem>
 
         <ListItem icon={<TempIcon />} title={t("commonWords.symptoms")} editAction={patientUIStore.goToReportSymptoms} hideEdit={props.pastReport}>
             <SymptomList symptoms={props.selectedSymptoms} />
+        </ListItem>
+        <ListItem icon={<FaceIcon />} title={"Support"} editAction={patientUIStore.goToReportSymptoms} hideEdit={props.pastReport}>
+            Feeling Okay
         </ListItem>
         <PhotoListItem pastReport={props.pastReport} missingPhoto={props.missingPhoto} isPhotoDay={props.isPhotoDay} photoString={props.photoString} />
     </div>)
@@ -158,7 +167,7 @@ const PhotoListItem = (props) => {
 function ListItem(props) {
 
     const classes = useStyles();
-    const {t} = useTranslation();
+    const { t } = useTranslation();
 
     return (
         <div className={`${classes.parent}`}>
@@ -166,7 +175,10 @@ function ListItem(props) {
                 <div className={classes.itemHeader}>
                     {props.icon}
                     {props.title}
-                    {!props.hideEdit && <Button className={classes.edit} onClick={props.editAction}><EditIcon /><span>{t('commonWords.edit')}</span></Button>}
+                    {!props.hideEdit && <Button className={classes.edit} onClick={props.editAction}>
+                        {props.first &&<span>{ t('commonWords.edit')}</span>}
+                        <EditIcon />
+                    </Button>}
                 </div>
                 <div className={classes.itemBody}>
                     {props.negative ? <ClearIcon className={classes.negative} /> : <CheckIcon className={classes.check} />}
--- src/DataStores/reportStore.js
+++ src/DataStores/reportStore.js
@@ -3,48 +3,6 @@ import { DateTime } from 'luxon';
 
 export default class ReportStore {
 
-
-    /* 
-        Offline Flow:
-
-        Each step of the flow uses the individual methods in here
-        
-        If the request doesn't go through because the user is not online, then we should cache that users input for the day
-
-
-        After each request update the object for a given day:
-
-
-        Change these methods to take in a report - that way you can pass it the report from the root store or you can pass it a report saved locally:
-        getMedicationBody()
-
-        "10-19-2021": {
-            photoReport
-            medicationReport
-            symptomReport
-        }
-
-
-        Another option: just save each submission as its own type and upload them individually, the downside to this is the number of requests, ie each report would then be 3 requests miniumum,
-        but then data is not lost when a patient edits thier reports multiple times
-
-
-        Easier option:
-
-
-        When in offline mode could fall back to a previous version of the  actionbox component that tracks the status based on the locally saved report.
-        When in offline, launch the old report flow that does not submit the report until the end of the flow
-
-
-
-        ********* :)
-
-        Can use new status part of todaysReportFrom server to capture if the aspect of the offline report has been captured or not
-        
-    
-    
-    */
-
     constructor(rootStore) {
         this.rootStore = rootStore;
     }
@@ -55,22 +13,26 @@ export default class ReportStore {
         this.todaysReportFromServer = report;
     }
 
-    @computed get baseReportComplete(){
-        return this.todaysReportFromServer.status && 
-        this.todaysReportFromServer.status.medicationReport &&
-        this.todaysReportFromServer.status.symptomReport  
+    @computed get baseReportComplete() {
+        return this.todaysReportFromServer &&
+            this.todaysReportFromServer.status &&
+            this.todaysReportFromServer.status.medicationReport &&
+            this.todaysReportFromServer.status.symptomReport
     }
 
-    @computed get allReportComplete(){
-        return this.todaysReportFromServer.status &&
-        this.todaysReportFromServer.status.complete
+    @computed get allReportComplete() {
+        return this.todaysReportFromServer &&
+            this.todaysReportFromServer.status &&
+            this.todaysReportFromServer.status.complete
     }
 
-    @computed get photoReportComplete(){
-        return this.todaysReportFromServer.status && this.todaysReportFromServer.status.photoReport
+    @computed get photoReportComplete() {
+        return this.todaysReportFromServer &&
+            this.todaysReportFromServer.status && 
+            this.todaysReportFromServer.status.photoReport
     }
 
-    @action processReport = (report) =>{
+    @action processReport = (report) => {
         this.todaysReportFromServer = report;
     }
 
