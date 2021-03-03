47c2e3bfac382597b87f60eb0fc58e4615a5e814
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-03-03 11:50:17 -0500

    Improve forgot password button visibility and page navigation

--- src/Login/ForgotPassword.js
+++ src/Login/ForgotPassword.js
@@ -1,20 +1,40 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
+import { useTranslation } from 'react-i18next';
+import Button from '@material-ui/core/Button';
+import useStores from '../Basics/UseStores';
+
 
 const useStyles = makeStyles({
-  container:{
-      width: "80%",
-      margin: "auto",
-      color: "white"
-  }
+    container: {
+        width: "80%",
+        margin: "auto",
+        color: "white",
+        height: "auto"
+    },
+    button: {
+        marginTop: "3em",
+        color: "white"
+    },
+    buttonContainer: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "flex-end"
+    }
 })
 
 const ForgotPassword = () => {
-
+    const { t } = useTranslation('translation');
     const classes = useStyles();
+    const { loginStore } = useStores();
 
-    return(<div className={classes.container}>
-        Contact your coordinator on WhatsApp, they will send you a code you can use to login and create a new password.
+    return (<div className={classes.container}>
+        {t('login.forgotPasswordDetails')}
+        <div className={classes.buttonContainer}>
+        <Button onClick={loginStore.selectPatient} className={classes.button} >
+                {t('patient.report.symptoms.warning.button')}
+            </Button>
+        </div>
     </div>)
 
 }
