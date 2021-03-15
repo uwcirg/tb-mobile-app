d121b5ec0912e4806c1bf3aa8f1fd0cd87502c88
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-03-15 11:40:06 -0400

    Add warning text to timeline

--- src/Patient/Home/Education/index.js
+++ src/Patient/Home/Education/index.js
@@ -7,11 +7,12 @@ import Styles from '../../../Basics/Styles';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 import Colors from '../../../Basics/Colors';
-import {observer} from 'mobx-react';
+import { observer } from 'mobx-react';
 import useStores from '../../../Basics/UseStores';
 import { useTranslation } from 'react-i18next';
 import raw from "raw.macro";
-
+import { usePageVisibility } from '../../../Hooks/PageVisibility'
+import { toJS } from 'mobx';
 const file = raw("../../../Content/TreatmentMessages.json");
 
 const useStyles = makeStyles({
@@ -19,7 +20,7 @@ const useStyles = makeStyles({
         width: "80%",
         minHeight: "80vh",
         ...Styles.flexColumn
-        
+
     },
     body: {
         maxHeight: "60vh",
@@ -28,7 +29,7 @@ const useStyles = makeStyles({
         "& > p": {
             textAlign: "left",
         },
-        "& > h2":{
+        "& > h2": {
             fontWeight: "bold",
             fontSize: "1em",
             textAlign: "left"
@@ -41,17 +42,17 @@ const useStyles = makeStyles({
         marginTop: "1em",
         textTransform: "capitalize"
     },
-    thumbsContainer:{
+    thumbsContainer: {
         width: "100%",
-        "& > p":{
+        "& > p": {
             fontWeight: "bold"
         }
     },
-    buttonGroup:{
+    buttonGroup: {
         alignSelf: "center",
         width: "90%",
         marginTop: "auto",
-        "& > button":{
+        "& > button": {
             width: "50%",
             color: Colors.buttonBlue
         }
@@ -62,48 +63,73 @@ const messages = JSON.parse(file)
 
 const EducationalMessage = observer((props) => {
 
-    const { t} = useTranslation('translation');
+    const { t } = useTranslation('translation');
 
     const classes = useStyles();
-    const {patientUIStore, patientStore} = useStores();
-    const  {educationStore: education} = useStores().patientStore;
-    const [visible,setVisible] =  useState(false);
+    const { patientUIStore, patientStore } = useStores();
+    const { educationStore: education } = useStores().patientStore;
+
+    const isVisible = usePageVisibility();
+    const [visible, setVisible] = useState(false);
+
+    //Check for service worker update when page goes from invisible to visible.
+    //this helps us detect when the application is launched from installed
+    useEffect(() => {
+        if (document.visibilityState === "visible") {
+            console.log("page visible opened")
+        }
+
+    }, [isVisible])
 
 
-    useEffect(()=>{
+    useEffect(() => {
+        // console.log(education.messageNumber);
+        // console.log(Object.keys(messages));
+        // console.log("Education Status")
+        // console.log(toJS(education.educationStatus));
+
+        if (education.educationStatus.length > 0 ) {
+            Object.keys(messages).map((messageNumber) => {
+                if (messageNumber <= education.educationStatus[education.educationStatus.length - 1]) {
+                    console.log(messageNumber)
+                }
+
+            })
+        }
+
         setVisible(patientStore.patientInformation.loaded && education.messageNumber >= 0 && messages[education.messageNumber] != null)
-    },[education.messageNumber,patientStore.patientInformation.loaded])
+    }, [education.educationStatus])
 
 
     const handleClose = (isExit) => {
         setVisible(!visible)
-        if(isExit) education.markEducationAsRead();
+        if (isExit) education.markEducationAsRead();
     }
 
     const handleRate = (rate) => {
         setVisible(!visible)
         education.markEducationAsRead(rate);
-        patientUIStore.setAlert(t("educationalMessages.feedback"),"success")
+        patientUIStore.setAlert(t("educationalMessages.feedback"), "success")
     }
 
     return (
         <>
-        {visible && !patientUIStore.onWalkthrough ?
-            <PopUp className={classes.container} handleClickAway={handleClose}>
-                <Typography className={classes.header} variant="h1">{t("educationalMessages.header")}: {t("time.week")} {Math.round(education.messageNumber / 7)}</Typography>
-                <div className={classes.body}>
-                    <p>{messages[education.messageNumber]}</p>
-                </div>
-
-                <div className={classes.thumbsContainer}>
-                    <p>{t("educationalMessages.helpful")}</p>
-                <ButtonGroup className={classes.buttonGroup}>
-                       <Button onClick={() =>{handleRate(false)}}> <ThumbDownIcon /></Button>   
-                       <Button onClick={() =>{handleRate(true)}}><ThumbUpIcon /></Button>
-                    </ButtonGroup>
+            {visible && !patientUIStore.onWalkthrough ?
+                <PopUp className={classes.container} handleClickAway={handleClose}>
+                    <Typography className={classes.header} variant="h1">{t("educationalMessages.header")}: {t("time.week")} {Math.round(education.messageNumber / 7)}</Typography>
+                    <div className={classes.body}>
+                        <p>{messages[education.messageNumber]}</p>
                     </div>
-            </PopUp> : ""}
-            </>)
+
+                    <div className={classes.thumbsContainer}>
+                        <p>{t("educationalMessages.helpful")}</p>
+                        <ButtonGroup className={classes.buttonGroup}>
+                            <Button onClick={() => { handleRate(false) }}> <ThumbDownIcon /></Button>
+                            <Button onClick={() => { handleRate(true) }}><ThumbUpIcon /></Button>
+                        </ButtonGroup>
+                    </div>
+                </PopUp> : ""}
+        </>)
 
 })
 
