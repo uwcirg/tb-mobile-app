255bdea25927311b4fe39470857c64f65d4d4b5f
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-07-01 15:38:18 -0400

    Layout for notification warning working

--- src/Patient/Home/PushEnrollmentReminder.js
+++ src/Patient/Home/PushEnrollmentReminder.js
@@ -1,29 +1,67 @@
 import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import useStores from '../../Basics/UseStores';
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import Colors from '../../Basics/Colors';
+import WarningIcon from '@material-ui/icons/WarningRounded'
+import ProfileButton from '../../Practitioner/PatientProfile/ProfileButton';
 
 const useStyles = makeStyles({
     warningContainer: {
-        backgroundColor: Colors.accentBlue,
-        padding: "2em",
+        backgroundColor: "white",
+        padding: "1.5em 2em",
         boxSizing: "border-box",
-        width: "100%"
+        width: "100%",
+        boxShadow: "inset 0px -27px 24px -27px rgba(0,0,0,0.5)",
+        "& > *": {
+            // marginBottom: ".5em"
+        },
+        "& > ul": {
+            margin: 0,
+            marginBottom: "1em",
+            padding: "0 0 0 1em"
+        },
+        "& > p, & > span": {
+            padding: ".5em"
+        }
     },
     button: {
-        alignSelf: "flex-end"
+        alignSelf: "flex-end",
+        textTransform: "capitalize"
+    },
+    title: {
+        display: "flex",
+        fontSize: "1.2em",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: Colors.warningRed,
+        padding: ".5em",
+        color: "white",
+        borderRadius: "5px"
     }
 })
 
+/*
+
+Plan: Parse Notificaiton Status,
+display message that depends on that
+state
+
+
+Notification State:
+- Unsupported (ie iOS)
+- Blocked
+- Enabled
+- Pending 
+
+*/
 
 const PushEnrollmentReminder = () => {
 
     //Default to true so that its not flickering for happy path
     const [enabled, setEnabled] = useState(true);
-    const [blocked, setBlocked] = useState(false);
+    const [notificationState, setNotificationState] = useState(false);
 
     useEffect(() => {
         setEnabled(checkPushEnabled());
@@ -39,7 +77,6 @@ const PushEnrollmentReminder = () => {
         });
     }
 
-
     const checkPushEnabled = () => {
         if (!("Notification" in window)) {
             console.log("This browser does not support desktop notification");
@@ -50,10 +87,10 @@ const PushEnrollmentReminder = () => {
         }
 
         // Otherwise, we need to ask the user for permission
-        else if (Notification.permission !== 'denied'){
+        else if (Notification.permission !== 'denied') {
             setBlocked(true);
         }
-        
+
         else if (Notification.permission === "default") {
             return false;
         }
@@ -61,10 +98,16 @@ const PushEnrollmentReminder = () => {
     }
 
     return (<>
-        {!enabled && <Grid direction="column" alignContent="center" className={classes.warningContainer} container spacing={1}>
-            <Typography variant="body1">It looks like your notiifcations are turned off</Typography>
-            <Typography variant="body1">Notifications help remind you about when to take your medication, or submit a photo</Typography>
-            <Button className={classes.button} onClick={enableFlow}>Click here to Enable</Button>
+        {!enabled && <Grid direction="column" className={classes.warningContainer} container spacing={1}>
+            <Typography className={classes.title} variant="h2">Notifications are off <WarningIcon /></Typography>
+            <Typography variant="body1">Push notifications are turned off. <br /> You will not recieve:</Typography>
+            <ul>
+                <li>  <Typography variant="body1">Medication Reminders</Typography></li>
+                <li>  <Typography variant="body1">Appointment Reminders</Typography></li>
+                <li>  <Typography variant="body1">New Message Alerts</Typography></li>
+            </ul>
+            <ProfileButton className={classes.button} onClick={enableFlow}>Click here to Enable</ProfileButton>
+            {/* <Button className={classes.button} onClick={enableFlow}>Click here to Enable</Button> */}
         </Grid>}
     </>)
 
