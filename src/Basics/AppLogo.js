0d57848c7c62160bc3ce15f858d0a9efc0d8d936
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-07-15 10:50:00 -0400

    Style fixup on login page

--- src/Basics/AppLogo.js
+++ src/Basics/AppLogo.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
-import {useTranslation} from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 
 const AppLogo = (props) => {
-    const { t } = useTranslation('translation');
-    const BASE_URL = (window && window._env) ? window._env.URL_CLIENT : ""
+  const { t } = useTranslation('translation');
+  const BASE_URL = (window && window._env) ? window._env.URL_CLIENT : ""
 
-    return(
+  return (
     <Title className={props.className}>
-    <img src={`${BASE_URL}/${props.white ? "logo-white.png" : "logo.png"}`}></img>
-    <h1>{t("title")}</h1>
+      <img src={`${BASE_URL}/${props.white ? "logo-white.png" : "logo.png"}`}></img>
+      <h1>{t("title")}</h1>
     </Title>
-    )
+  )
 }
 
 const Title = styled.div`
