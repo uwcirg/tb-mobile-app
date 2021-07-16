bd29fdb62564095c87a71503cdfd4374171c5e78
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-07-16 11:22:37 -0400

    Cleanup layout of med reminder card

--- src/Patient/Home/MedicationReminder.js
+++ src/Patient/Home/MedicationReminder.js
@@ -9,26 +9,28 @@ import Button from "@material-ui/core/Button";
 import { DateTime } from 'luxon';
 import Colors from '../../Basics/Colors';
 import AddReminder from './Reminder/index';
-import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import AccessAlarmIcon from '@material-ui/icons/AlarmOn';
 import TimeDialog from '../../Components/TimeDialog';
-import EditIcon from '@material-ui/icons/Edit';
 import Grid from '@material-ui/core/Grid';
-import CancelIcon from '@material-ui/icons/Cancel';
+import ClickableText from '../../Basics/ClickableText';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 
 
 const useStyles = makeStyles({
-    header: { fontSize: "1em", fontWeight: "bold", textAlign: "left", width: "100%", paddingLeft: "1em" },
     daily: {
         width: "100%",
-        padding: "0 1em 1em 1em",
         boxSizing: "border-box"
     },
     options: {
         color: Colors.buttonBlue,
-        fontSize: "3em",
+        fontSize: "2.5em",
         padding: 0,
-        margin: 0
+        margin: 0,
+    },
+    icon: {
+        color: Colors.buttonBlue,
+        fontSize: "2.5em"
     },
     reminder: { padding: "1em 1em 0 1em" },
     bottomLabel: {
@@ -38,7 +40,7 @@ const useStyles = makeStyles({
         textAlign: "center"
     },
     button: {
-        "& > svg":{
+        "& > svg": {
             fontSize: ".75em"
         },
         display: "flex",
@@ -52,6 +54,18 @@ const useStyles = makeStyles({
         color: props => props.color || Colors.buttonBlue,
         padding: ".5em",
         fontSize: "1em",
+    },
+    reminderText: {
+        padding: "0",
+        lineHeight: "1.2em",
+        maxWidth: "70px"
+    },
+    menuContainer: {
+        padding: "1em"
+    },
+    top: {
+        padding: "0 1em",
+        width: "100%"
     }
 
 })
@@ -85,15 +99,23 @@ const Card = observer(() => {
     return (<InteractionCard upperText={<><AccessAlarmIcon />{t('patient.reminders.medicationReminder')}</>} id="intro-reminders-card">
         <div className={classes.daily}>
             {patientStore.reminderTime ? <>
-                <Grid container wrap="nowrap" alignItems="center">
-                    <Typography variant="body1">Reminder will be be sent daily at </Typography>
-                    <Typography className={classes.options} variant="body1">{DateTime.fromISO(patientStore.reminderTime).toLocaleString(DateTime.TIME_24_SIMPLE)}</Typography>
+                <Grid container wrap="nowrap" alignItems="center" justify="space-between"  className={classes.top}>
+                        <AccessAlarmIcon className={classes.icon} />
+                        <Typography className={classes.reminderText} variant="body1">Reminder Enabled</Typography>    
+                        <Typography align="center" className={classes.reminderText} variant="body1">at</Typography>
+                        <Typography className={classes.options} variant="body1">{DateTime.fromISO(patientStore.reminderTime).toLocaleString(DateTime.TIME_24_SIMPLE)}</Typography>
                 </Grid>
-                <Grid container justify="space-around">
+
+                <Grid className={classes.menuContainer} container justify="flex-end">
+                    <ClickableText icon={<MoreVertIcon style={{ fontSize: "1.2em" }} />} text="Edit or Disable"></ClickableText>
+                </Grid>
+
+
+                {/* <Grid container direction="column" justify="space-around">
                     <Option onClick={() => { setOpen(true) }}><EditIcon /> {t('patient.reminders.changeTime')}</Option>
                     <Grid item xs={1} />
                     <Option color={Colors.red} onClick={() => { patientStore.updateNotificationTime(true) }}> <CancelIcon />{t('patient.reminders.disable')}</Option>
-                </Grid>
+                </Grid> */}
             </> : <Grid alignItems="center" container>
                 <Grid item xs={8}>{t('patient.reminders.explanation')}</Grid>
                 <Grid item xs={4} >
@@ -115,7 +137,7 @@ const Card = observer(() => {
 })
 
 const Option = (props) => {
-    const classes = useStyles({color: props.color});
+    const classes = useStyles({ color: props.color });
     return <Button {...props} disableElevation className={classes.button} />
 }
 
