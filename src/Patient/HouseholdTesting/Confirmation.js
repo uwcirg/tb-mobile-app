c1dae17d158a6854d6087976743877fc131d637f
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-08-16 12:34:46 -0400

    Style messages on confirmation page, disallow crawling in robots.txt

--- src/Patient/HouseholdTesting/Confirmation.js
+++ src/Patient/HouseholdTesting/Confirmation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ConfirmationLayout from '../../Components/Patient/ConfirmationLayout';
 import { makeStyles } from '@material-ui/core/styles';
 import useStores from '../../Basics/UseStores';
 import Typography from '@material-ui/core/Typography';
@@ -15,15 +16,23 @@ const useStyles = makeStyles({
         minHeight: "60vh",
         "& > *": {
             marginBottom: "1em",
-            textAlign: "left"
+            textAlign: "left",
+            padding: ".5em",
+            borderRadius: "5px"
         }
+    },
+    success:{
+        backgroundColor: Colors.calendarGreen
+    },
+    reachOut:{
+        backgroundColor: Colors.highlightYellow,
+
     }
 })
 
-const ConfirmationScreen = ({ nContacts, nTested, wasError, handleNext }) => {
+const ConfirmationScreen = ({ numberOfTests, numberOfContacts, wasError, handleNext }) => {
 
     const { uiStore, patientStore } = useStores();
-    const classes = useStyles();
     const { t } = useTranslation('translation');
 
     const completeSurvey = () => {
@@ -31,14 +40,14 @@ const ConfirmationScreen = ({ nContacts, nTested, wasError, handleNext }) => {
         patientStore.initalize();
     }
 
-    const completed = nTested >= nContacts;
+    const completed = numberOfTests >= numberOfContacts;
 
-    const body = wasError ? <Error /> : <Success completed />
+    const body = wasError ? <Error /> : <Success completed={completed} />
 
     return (
         <>
             {body}
-            <NextButton onClick={handleNext} text={t('archive.complete')} />
+            <NextButton onClick={completeSurvey} text={t('patient.report.symptoms.warning.button')} />
         </>
     )
 }
@@ -50,22 +59,32 @@ const Success = ({ completed }) => {
 
     return (
         <Grid container className={classes.body} direction="column" justify="center" align="center">
-            <Grid container alignItems="center">
-                <Check style={{ color: Colors.green, marginRight: ".5em" }} />
-                <Typography>{t('householdTesting.recorded')}</Typography>
-            </Grid>
 
-            <Typography variant="body1" color="initial">
-                {t('householdTesting.thanks')}
-            </Typography>
-            <Typography variant="body1" color="initial">
-                {!completed ? "It is great that all of your contacts have been tested. Keep up the great work." :
-                    <>
-                        <Typography variant="body1" color="initial">Its very important for the health of your loved ones to ensure that they all get tested for TB.</Typography>
-                        <Typography style={{ backgroundColor: Colors.highlightYellow }}>{t('householdTesting.reachOut')}</Typography>
-                    </>}
-            </Typography>
+            <ConfirmationLayout title={t('commonWords.successMessage')} subtitle={t('householdTesting.recorded')} />
+            <Typography variant="body1" color="initial">{t('householdTesting.thanks')}</Typography>
+            {completed ? <CompleteMessage /> : <IncompleteMessage />}
         </Grid>
+    )
+}
+
+const CompleteMessage = () => {
+    const { t } = useTranslation('translation');
+    const classes = useStyles();
+    return(
+        <Typography className={classes.success} variant="body1">{t('householdTesting.completed')}</Typography>
+    )
+    
+}
+
+const IncompleteMessage = () => {
+     const { t } = useTranslation('translation');
+     const classes = useStyles();
+
+    return (
+    <>
+        <Typography variant="body1">{t('householdTesting.explanation')}</Typography>
+        <Typography variant="body1" className={classes.reachOut}>{t('householdTesting.reachOut')}</Typography>
+    </>
     )
 }
 
