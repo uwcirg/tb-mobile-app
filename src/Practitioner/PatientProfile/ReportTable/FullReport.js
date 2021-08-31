7f0e706f065fceaf777ba6781e8c38b2040c528e
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-08-31 18:13:04 -0400

    Touch up desing of drop down full report

--- src/Practitioner/PatientProfile/ReportTable/FullReport.js
+++ src/Practitioner/PatientProfile/ReportTable/FullReport.js
@@ -8,10 +8,14 @@ import { useTranslation } from 'react-i18next';
 import { DateTime } from 'luxon';
 import Symptom from '../../Shared/Symptom';
 import Colors from '../../../Basics/Colors';
+import ImagePopUp from '../../Shared/ImagePopUp';
+import ClickableText from '../../../Basics/ClickableText';
+import ExpandIcon from '@material-ui/icons/AspectRatio';
+import useToggle from '../../../Hooks/useToggle';
 
 const useStyles = makeStyles({
     body: {
-        "& > tr:nth-of-type(odd)": {
+        "& > tr:nth-of-type(odd) > td:nth-of-type(2)": {
             backgroundColor: Colors.lighterGray
         },
         "& > tr > td:first-of-type": {
@@ -31,11 +35,16 @@ const useStyles = makeStyles({
     },
     photo: {
         width: "100px"
+    },
+    capitalize:{
+        textTransform: "capitalize"
     }
 })
 
 const FullSymptomList = (props) => {
+    
     const { t } = useTranslation('translation');
+
     return (<>
         {props.list.length > 0 ? <div> {props.list.map(symptom => {
             return (<p key={`report-symptom-list-${symptom}`}><Symptom icon string={symptom} /></p>)
@@ -46,11 +55,14 @@ const FullSymptomList = (props) => {
 const FullReport = ({ row }) => {
 
     const { t } = useTranslation('translation');
+    const [expand,toggleExpanded] = useToggle(false);
     const classes = useStyles();
 
     const timeTaken = DateTime.fromISO(row.takenAt).toLocaleString(DateTime.TIME_SIMPLE)
 
-    return (<Table size="small" aria-label="report-details">
+    return (
+    <>
+    <Table size="small" aria-label="report-details">
         <TableBody className={classes.body}>
             <TableRow>
                 <TableCell>{t('commonWords.medication')}</TableCell>
@@ -68,14 +80,18 @@ const FullReport = ({ row }) => {
                 <TableRow>
                     <TableCell>{t('patient.report.doingWell')}:</TableCell>
                     <TableCell>
-                        <p>{row.doingOkay ? "Yes" : "Need Support"}</p>
-                        {row.doingOkayReason && <p>Reason: {row.doingOkayReason}</p>}
+                        <p className={classes.capitalize}>{row.doingOkay ? t('commonWords.yes') : t('patient.report.needSupport')}</p>
+                        {row.doingOkayReason && <p>{t('coordinator.message')}: {row.doingOkayReason}</p>}
                     </TableCell>
                 </TableRow>
                 <TableRow>
                     <TableCell>{t('coordinator.patientProfile.photo')}:</TableCell>
                     <TableCell>
-                        {!row.photoWasRequired ? <p>{t('report.photoNotNeeded')}</p> : <>{row.photoUrl ? <img className={classes.photo} src={row.photoUrl} /> : <p>Not Submitted</p>}</>}
+                        {!row.photoWasRequired ? <p>{t('report.photoNotNeeded')}</p> : <>{row.photoUrl ? <>
+                        <img className={classes.photo} src={row.photoUrl} />
+                        <br />
+                        <ClickableText onClick={toggleExpanded} hideIcon text={<><ExpandIcon className={classes.expandIcon} />{t('coordinator.sideBar.expandPhoto')}</>} />
+                        </> : <p>{t('report.missedPhoto')}</p>}</>}
                     </TableCell>
                 </TableRow>
                 <TableRow>
@@ -83,7 +99,10 @@ const FullReport = ({ row }) => {
                     <TableCell>{DateTime.fromISO(row.createdAt).toLocaleString(DateTime.DATETIME_SHORT)}</TableCell>
                 </TableRow>
         </TableBody>
-    </Table>)
+    </Table>
+    {expand && <ImagePopUp close={toggleExpanded} imageSrc={row.photoUrl} />}
+    </>
+    )
 }
 
         export default FullReport;
