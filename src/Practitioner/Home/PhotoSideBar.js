ca50f1ca60fdd4fca436dd9af3c1e0c6a50dab6e
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-09-10 11:26:21 -0700

    Change styles of buttons and add photo report details to sidebar

--- src/Practitioner/Home/PhotoSideBar.js
+++ src/Practitioner/Home/PhotoSideBar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Basicsidebar from '../Shared/BasicSidebar'
 import useStores from '../../Basics/UseStores'
 import { observer } from 'mobx-react'
@@ -11,6 +11,9 @@ import { useTranslation } from 'react-i18next';
 import ImagePopUp from '../Shared/ImagePopUp';
 import ClickableText from '../../Basics/ClickableText';
 import ExpandIcon from '@material-ui/icons/AspectRatio';
+import Label from '../../Components/Label';
+import { Typography } from '@material-ui/core';
+import { DateTime } from 'luxon';
 
 const useStyles = makeStyles({
 
@@ -35,36 +38,45 @@ const useStyles = makeStyles({
         ...Styles.flexRow,
         justifyContent: "space-evenly"
     },
-    expand:{
+    expand: {
         width: "100%",
         display: "flex",
         paddingLeft: "10%",
         marginTop: "1em"
     },
-    expandIcon:{
+    expandIcon: {
         marginRight: "5px"
+    },
+    lateArea:{
+        width: "100%",
+        padding: "1em",
+        boxSizing: "border-box"
+    },
+    label:{
+        width: "fit-content",
+        marginBottom: ".5em"
     }
 })
 
 const PhotoSidebar = observer(() => {
-    const [expand,setExpand] = useState(false);
+    const [expand, setExpand] = useState(false);
 
     const { practitionerStore } = useStores();
     const classes = useStyles();
-    const {t} = useTranslation('translation');
+    const { t } = useTranslation('translation');
     const item = practitionerStore.filteredPatients.photo[practitionerStore.selectedRow.index];
 
-    const toggleExpanded = () => { setExpand(!expand)}
+    const toggleExpanded = () => { setExpand(!expand) }
 
     return (
         <Basicsidebar buttons={
             <>
-                <SharedButton text={t('report.inconclusive')} onClick={() => { practitionerStore.processPhoto(item.photoId, false) }} color={Colors.yellow} icon={<QIcon />} />
+                <SharedButton text={t('report.inconclusive')} onClick={() => { practitionerStore.processPhoto(item.photoId, false) }} color={Colors.warningRed} icon={<QIcon />} />
                 <SharedButton text={t('report.conclusive')} onClick={() => { practitionerStore.processPhoto(item.photoId, true) }} />
             </>}>
             <div className={classes.photoContainer} >
                 <h2>{t("coordinator.sideBar.photoSub")}:</h2>
-                {item.backSubmission && <p>Back Submission</p>}
+                {item.backSubmission && <LateSubmissionInfo photoReport={item} />}
                 <img className={classes.photoPreview} src={item.url} />
                 <div className={classes.expand}>
                     <ClickableText onClick={toggleExpanded} hideIcon text={<><ExpandIcon className={classes.expandIcon} />{t('coordinator.sideBar.expandPhoto')}</>} />
@@ -74,6 +86,23 @@ const PhotoSidebar = observer(() => {
         </Basicsidebar>
     )
 });
+
+const LateSubmissionInfo = ({photoReport}) => {
+    const { t } = useTranslation('translation');
+    const classes = useStyles();
+
+    const requestDate = DateTime.fromISO(photoReport.date).toLocaleString(DateTime.DATE_MED)
+    const submittedDatetime = DateTime.fromISO(photoReport.createdAt).toLocaleString(DateTime.DATETIME_MED)
+
+    return (
+        <div className={classes.lateArea}>
+            <Label className={classes.label} backgroundColor={Colors.yellow} text={"Submitted Late"} />
+            <Typography><strong>{t('dashboard.requested')}:</strong> {requestDate}</Typography>
+            <Typography><strong>{t('dashboard.submitted')}:</strong> {submittedDatetime}</Typography>
+        </div>
+    )
+
+}
 
 
 export default PhotoSidebar;
