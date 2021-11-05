5bdb907f6dd3f4f59c34a2fccaa48bb5327f6c70
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-11-05 14:23:53 -0400

    Fix UX of yes / no medicaiton question

--- src/Patient/ReportingFlows/ReportMedication.js
+++ src/Patient/ReportingFlows/ReportMedication.js
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { observer } from 'mobx-react';
 import { useTranslation } from 'react-i18next';
 import Fade from '@material-ui/core/Fade';
 import useStores from '../../Basics/UseStores';
 import TextField from '@material-ui/core/OutlinedInput';
-import { ButtonBase, makeStyles, Typography } from '@material-ui/core';
+import { ButtonBase, FormControl, FormControlLabel, FormLabel, makeStyles, Radio, RadioGroup, Typography } from '@material-ui/core';
 import SimpleButton from '../../Basics/SimpleButton';
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
 import Colors from '../../Basics/Colors';
-import Check from '@material-ui/icons/Check';
-import Clear from '@material-ui/icons/Clear';
+import Check from '@material-ui/icons/CheckRounded';
+import Clear from '@material-ui/icons/ClearRounded';
 
 
 const useStyles = makeStyles({
@@ -38,8 +38,11 @@ const useStyles = makeStyles({
     reasonContainer: {
         padding: "0 1em"
     },
-    inputLabel:{
+    inputLabel: {
         padding: ".5em 0"
+    },
+    radioGroup: {
+        color: "blue"
     }
 });
 
@@ -52,7 +55,7 @@ const ReportMedication = observer((props) => {
     const tookMedication = patientStore.report.tookMedication;
     const nextEnabled = tookMedication || patientStore.report.whyMedicationNotTaken.length > 0
 
-     patientStore.reportStore.setReportHeader(t("patient.report.didYouTake"));
+    patientStore.reportStore.setReportHeader(t("patient.report.didYouTake"));
 
     // useEffect(() => {
     //     patientStore.reportStore.setReportHeader(t("patient.report.didYouTake"));
@@ -69,10 +72,10 @@ const ReportMedication = observer((props) => {
         <div>
             <Fade timeout={1000} in={true}>
                 <Grid direction="column" container className={classes.selectionContainer} >
-                    <Grid justify="center" direction="column" className={classes.yesNoButtons} container>
-                        <SelectionButton value={true} handleClick={handleClick} selected={!!tookMedication} />
-                        <Box height="1em" />
-                        <SelectionButton value={false} handleClick={handleClick} selected={!!!tookMedication} />
+                    <Grid justify="center" className={classes.yesNoButtons} container>
+                        <SelectionButton value={true} handleClick={handleClick} selected={tookMedication === true} />
+                        <Box width="1em" />
+                        <SelectionButton value={false} handleClick={handleClick} selected={tookMedication === false} />
                     </Grid>
                     {!tookMedication && <div className={classes.reasonContainer}>
                         <Typography className={classes.inputLabel} variant="body1">{t('patient.report.whyNotTaken')}</Typography>
@@ -85,36 +88,55 @@ const ReportMedication = observer((props) => {
     )
 });
 
+
 const useButtonStyles = makeStyles({
     button: {
-        flex: 1,
+        overflow: "hidden",
+        flex: "1 1 0",
         padding: ".5em 1em",
         fontSize: "2em",
         borderRadius: "4px",
         textTransform: "capitalize",
-        // textDecoration: props => props.selected ? "underline" : "none",
-        border: props => props.selected ? `solid 1px ${Colors.accentBlue}` : "none",
+        backgroundColor: "none",
         "& .MuiSvgIcon-root": {
-            fontSize: "1em",
-            paddingRight: ".5em"
-        }
+            fontSize: "1.25em",
+            paddingBottom: ".5em",
+            color: props => !props.selected ? (props.value ? Colors.green : Colors.warningRed) : Colors.textDarkGray
+        },
+        boxSizing: "border-box"
     },
     yes: {
-        backgroundColor: Colors.calendarGreen
+        backgroundColor: props => props.selected ? Colors.calendarGreen : "white",
     },
     no: {
-        backgroundColor: Colors.calendarRed
+        backgroundColor: props => props.selected ? Colors.calendarRed : "white"
+    },
+    buttonBack: {
+        boxSizing: "border-box",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        zIndex: 0,
+        opacity: props => props.selected ? "1" : ".5",
+        border: props => props.selected ? "none" : `3px solid ${Colors.gray}`
+    },
+    buttonContent: {
+        zIndex: 1
+    },
+    yesIcon: {
+        color: props => !props.selected ? Colors.green : "white",
     }
 })
 
 const SelectionButton = ({ selected, value, handleClick }) => {
-    const classes = useButtonStyles({ selected: selected });
+    const classes = useButtonStyles({ selected: selected, value: value });
     const { t } = useTranslation();
-    return <ButtonBase className={`${classes.button} ${value ? classes.yes : classes.no}`} onClick={() => { handleClick(value) }}>
-        <Grid wrap="nowrap" alignItems="center" container>
+    return <ButtonBase className={`${classes.button}`} onClick={() => { handleClick(value) }}>
+        <Grid className={classes.buttonContent} wrap="nowrap" alignItems="center" direction="column" container>
             {value ? <Check /> : <Clear />}
             {value ? t('commonWords.yes') : t('commonWords.no')}
         </Grid>
+        <div className={`${classes.buttonBack} ${value ? classes.yes : classes.no}`}></div>
     </ButtonBase>
 }
 
