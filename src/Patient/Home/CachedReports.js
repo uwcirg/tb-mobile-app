9f24cb1cc1383ffe244ea8a3687feccac8dfd05a
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-12-14 08:40:19 -0500

    Improved UI for messaging about number of offline reports

--- src/Patient/Home/CachedReports.js
+++ src/Patient/Home/CachedReports.js
@@ -6,26 +6,23 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 import FinishedIcon from '@material-ui/icons/CheckCircle';
 import Colors from '../../Basics/Colors';
 import { useTranslation } from 'react-i18next';
+import Grid from '@material-ui/core/Grid'
+import { WifiOff } from '@material-ui/icons';
+import { Box } from '@material-ui/core';
 
 const useStyles = makeStyles({
     container: {
-        margin: "auto 1em auto 1em",
-        width: "80%",
+        padding: "1em 1.5em",
+        width: "100%",
+        boxSizing: "border-box"
     },
     message: {
-        textAlign: "center",
         padding: 0
-    },
-    progressVisual: {
-
-
     },
     finished: {
         display: "flex",
         alignItems: "center",
         margin: "1em 0 1em 0"
-
-
     }
 })
 
@@ -33,6 +30,8 @@ const CachedReports = observer(() => {
     const classes = useStyles();
     const { patientStore, dailyReportStore, uiStore } = useStores();
     const { t } = useTranslation('translation');
+
+    const shouldRender = (!uiStore.offline && dailyReportStore.finished) || (dailyReportStore.syncing) || (!dailyReportStore.syncing && dailyReportStore.numberOfflineReports > 0)
 
     useEffect(() => {
         dailyReportStore.getCachedReports();
@@ -44,19 +43,38 @@ const CachedReports = observer(() => {
         }
     }, [dailyReportStore.finished])
 
-    return (<div className={classes.container}>
-        {!uiStore.offline && dailyReportStore.finished ? <div className={classes.finished}>
-            {t('patient.report.offline.allDone')} <FinishedIcon style={{ color: Colors.approvedGreen, marginLeft: "5px" }} />
-        </div> : <>
-                {dailyReportStore.syncing && <LinearProgress
-                    className={classes.progressVisual}
-                />}
-                {!dailyReportStore.syncing && dailyReportStore.numberOfflineReports > 0 && <p className={classes.message}>
-                    {t('patient.report.offline.youHave')} {dailyReportStore.numberOfflineReports} {t('patient.report.offline.report', { count: dailyReportStore.numberOfflineReports })}. {t('patient.report.offline.explanation')}</p>}
-                {dailyReportStore.syncing && <p className={classes.message}>{t('patient.report.offline.uploading')}... </p>}
-            </>}
-    </div>)
+    return (
+        <>
+            {shouldRender && <div className={classes.container}>
+                {!uiStore.offline && dailyReportStore.finished ? <div className={classes.finished}>
+                    {t('patient.report.offline.allDone')} <FinishedIcon style={{ color: Colors.approvedGreen, marginLeft: "5px" }} />
+                </div> : <>
+                    {dailyReportStore.syncing && <LinearProgress
+                        className={classes.progressVisual}
+                    />}
+                    {!dailyReportStore.syncing && dailyReportStore.numberOfflineReports > 0 &&
+                        <OfflineMessage />}
+                    {dailyReportStore.syncing && <p className={classes.message}>{t('patient.report.offline.uploading')}... </p>}
+                </>}
+            </div>}
+        </>
+    )
 
 })
+
+const OfflineMessage = observer(() => {
+
+    const classes = useStyles();
+    const { dailyReportStore } = useStores();
+    const { t } = useTranslation('translation');
+
+    return (<Grid style={{ boxSizing: "border-box" }} alignItems="center" wrap="nowrap" container>
+        <WifiOff style={{ color: Colors.blue, paddingRight: "1em" }} />
+        <p className={classes.message}>
+            {t('patient.report.offline.youHave')} {dailyReportStore.numberOfflineReports} {t('patient.report.offline.report', { count: dailyReportStore.numberOfflineReports })}. {t('patient.report.offline.explanation')}
+        </p>
+    </Grid>)
+
+});
 
 export default CachedReports;
