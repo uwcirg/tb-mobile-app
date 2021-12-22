bd09f0905b0b09388819b34e83b0b7e2f4d56419
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-12-22 10:52:35 -0800

    Add new translations for card

---
+++ src/Patient/Home/OneStepReportingUpdate.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useTranslation } from 'react-i18next';
+
+const useStyles = makeStyles({
+
+})
+
+const OneStepReportingUpdate = () => {
+
+    const classes = useStyles();
+    const { t } = useTranslation('translation');
+
+    return (<div>
+
+    </div>)
+
+}
+
+export default OneStepReportingUpdate;
