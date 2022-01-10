e8864bedc2bc13817da60efe9a2cfcb9ab1a2bfe
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-01-10 18:58:51 -0500

    Progress on layout of patient photo list

--- src/Patient/Progress/PhotoList.js
+++ src/Patient/Progress/PhotoList.js
@@ -5,10 +5,15 @@ import useStores from '../../Basics/UseStores';
 import Colors from '../../Basics/Colors';
 import Grid from '@material-ui/core/Grid'
 import { Box, Typography } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
+import AspectRatioIcon from '@material-ui/icons/ZoomOutMap';
 
 const useStyles = makeStyles({
+    container: {
+        padding: "0 .5em"
+    },
     photo: {
-        backgroundColor: Colors.lighterGray,
+        backgroundColor: Colors.lightgray,
         height: "75px",
         width: "75px",
         backgroundSize: "fill",
@@ -16,8 +21,17 @@ const useStyles = makeStyles({
         backgroundRepeat: "no-repeat",
         borderRadius: "4px"
     },
-    item:{
-        padding: ".5em"
+    item: {
+        padding: ".5em",
+        backgroundColor: Colors.lighterGray
+    },
+    title: {
+        fontSize: "1.5em",
+        padding: ".5em 0"
+    },
+    expand:{
+        fontSize: "1em",
+        color: "white"
     }
 })
 
@@ -25,21 +39,24 @@ const PhotoList = observer(({ photos }) => {
 
     const classes = useStyles();
     const { patientStore } = useStores();
+    const { t } = useTranslation('translation');
 
-    return (<div>
+    return (<div className={classes.container}>
+        <Typography className={classes.title} variant="h2">{t('dashboard.photoReports')}</Typography>
         {patientStore.photoReports.map(photoReport => {
             return (
-                <>
-                <Grid className={classes.item} container>
-                    <Box flexGrow="1">
-                        <Typography>Date: </Typography>
-                    </Box>
-                    <div>{photoReport.date}</div>
-                    <div style={{ backgroundImage: `url(${photoReport.url})` }} className={classes.photo} />
-                    <Box width=".5em" />
-                </Grid>
-                <Box height=".5em" />
-                </>
+                <div key={`photo-list-${photoReport.id}`}>
+                    <Grid className={classes.item} container>
+                        <Box flexGrow="1">
+                            <Typography>{photoReport.date}</Typography>
+                        </Box>
+                        <div style={{ backgroundImage: `url(${photoReport.url})` }} className={classes.photo}>
+                            <AspectRatioIcon className={classes.expand} />
+                        </div>
+                        <Box width=".5em" />
+                    </Grid>
+                    <Box height=".5em" />
+                </div>
             )
         })}
     </div>)
