8bdbdb2b7ccfdabd2e3550508ec8d79f10f146a6
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-01-18 12:09:31 -0500

    Add new section for photo reports

---
+++ src/Practitioner/PatientProfile/PhotoReportsList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  
+})
+
+const PhotoReportsList = () => {
+
+    const classes = useStyles();
+
+    return(<div>
+        <p>Photos</p>
+    </div>)
+
+}
+
+export default PhotoReportsList;
