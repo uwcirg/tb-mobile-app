190ef6bd1e0d2f1aae5136fac87c9a7196ab6478
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-04-05 22:53:45 -0400

    Add outcode for refusal to use app

--- src/Basics/Enums.js
+++ src/Basics/Enums.js
@@ -1,1 +1,1 @@
-export const treatmentOutcomes =  ["success","default","transferred","deceased","lost-to-follow-up","withdraw"]
+export const treatmentOutcomes =  ["success","default","transferred","deceased","lost-to-follow-up","withdraw","refuse-to-use-app"]
