2cc37228adf8b0fa275a4495528960cf642b6c9d
johnny2smooth <johnbp13@gmail.com>
2022-05-19 15:02:57 -0700

    countdown timer bug

--- src/Patient/Home/TestSteps/Countdown.js
+++ src/Patient/Home/TestSteps/Countdown.js
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { observer } from 'mobx-react';
-import { Button, Card } from '@material-ui/core';
-import useCountdown from '../../../Hooks/useCountdown';
-import SimpleButton from '../../../Basics/SimpleButton';
-import { DateTime, Duration, Interval } from 'luxon';
-
-import usePersistCountdown from '../../../Hooks/usePersistCountdown';
+import { Satellite } from '@material-ui/icons';
+import { DateTime } from 'luxon';
 
 const useStyles = makeStyles({
   root: {
@@ -16,39 +12,56 @@ const useStyles = makeStyles({
     background: '#90EE90',
   },
 });
-localStorage.setItem(
-  'now',
-  DateTime.local().plus({ seconds: 1200 }).toString()
-);
 
 const Countdown = observer(() => {
+  // When it renders - grab the time that we are waiting for
+  const [state, setState] = useState({
+    laterTime: null,
+    currentTime: DateTime.local(),
+  });
+
+  useEffect(() => {
+    setState({
+      ...state,
+      laterTime: localStorage.getItem('later'),
+    });
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setState({
+        ...state,
+        currentTime: DateTime.local(),
+      });
+    }, 1000);
+    return function cleanup() {
+      clearInterval(interval);
+    };
+  }, []);
+
+  // Update once per second to new display time
+
   const classes = useStyles();
-  // const [second, minute, setCountdown] = useCountdown(20);
-  let now = DateTime.local();
-  let dur = Duration.fromObject({ seconds: 1200 });
-  let { count, later, nowish, setLocalTimes, interval, localCount } =
-    usePersistCountdown();
+
+  const setFutureTime = () => {
+    const newTime = DateTime.local().plus({ minutes: 20 }).toISO();
+    setState({
+      ...state,
+      laterTime: newTime,
+    });
+    localStorage.setItem('later', newTime);
+  };
+
+  const dif = DateTime.fromISO(state.laterTime).diff(
+    state.currentTime,
+    'seconds'
+  ).seconds;
 
   return (
     <div>
-      <Card className={classes.root}>
-        <h2>Please, use this timer</h2>
-        <p>
-          {/* {minute}:{second < 10 && '0'}
-          {second} */}
-          {now.toISO().toString()} now now now!
-        </p>
-        <p>{localStorage.getItem('later')}</p>
-        <p>{localStorage.getItem('now')} now + 20 minutes</p>
-        <p>{Number(dur.seconds) / 60} minutes</p>
-        <p>{interval} INTERVAL!</p>
-        <SimpleButton onClick={setLocalTimes}>Very simple</SimpleButton>
-      </Card>
-      <h1>{1200 - count}</h1>
-      <h2>{later}</h2>
-      <h2>{nowish}</h2>
-      <h3>{localStorage.getItem('count')}</h3>
-      <h4>{localCount}</h4>
+      <button onClick={setFutureTime}>Set Time</button>
+      {/* Display the time  */}
+      <p>{state.laterTime && Math.round(dif)}</p>
     </div>
   );
 });
