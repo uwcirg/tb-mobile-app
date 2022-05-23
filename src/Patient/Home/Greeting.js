37aac4782800d9e0d1435bd262544d3cd81019ee
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-05-23 15:34:46 -0400

    Add greeting message and basic tests

--- src/Patient/Home/Greeting.js
+++ src/Patient/Home/Greeting.js
@@ -2,24 +2,23 @@ import React, { useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import useStores from '../../Basics/UseStores';
 import { DateTime } from 'luxon';
-import Styles from '../../Basics/Styles';
 import Colors from '../../Basics/Colors';
 import { useTranslation } from 'react-i18next';
-import {observer} from 'mobx-react'
+import { observer } from 'mobx-react'
+import { Box, Grid } from '@material-ui/core';
+import { toJS } from 'mobx';
+
+import motivationalMessages from '../../Content/motivational-messages';
 
 const useStyles = makeStyles({
-  container:{
-      ...Styles.flexRow,
-      justifyContent: "flex-start",
-      alignItems: "center",
-      width: "85%",
-      margin: "1em 0"
+  container: {
+
   },
-  greeting:{
+  greeting: {
     fontWeight: "medium",
     fontSize: "1.25em"
   },
-  date:{
+  date: {
     marginLeft: "auto",
     textTransform: "uppercase",
     color: Colors.textGray,
@@ -31,14 +30,31 @@ const Greeting = observer(() => {
 
   const { t } = useTranslation('translation');
 
-    const classes = useStyles();
-    const {patientStore} = useStores();
+  const classes = useStyles();
+  const { patientStore } = useStores();
 
-    return(<div id="intro-greeting" className={classes.container}>
-        <div className={classes.greeting}>{t("greeting")} {patientStore.givenName} 👋 </div> 
-        <div className={classes.date}>{DateTime.fromISO(patientStore.reportStore.todaysDate).toLocaleString(DateTime.DATE_FULL)}</div>
-    </div>)
+  const isIndonesia = process.env.REACT_APP_IS_INDONESIA === 'true';
+
+  return (<Box width="100%" padding="1em" style={{ boxSizing: "border-box" }} id="intro-greeting">
+    <Grid container>
+      <div className={classes.greeting}>{t("greeting")} {patientStore.givenName} 👋 </div>
+      <Box flexGrow={1} />
+      <div className={classes.date}>{DateTime.fromISO(patientStore.reportStore.todaysDate).toLocaleString(DateTime.DATE_FULL)}</div>
+    </Grid>
+    {isIndonesia && <MotivationalMessage treatmentDay={patientStore.patientInformation.daysInTreatment} />}
+  </Box>)
 
 })
+
+const MotivationalMessage = ({ treatmentDay }) => {
+
+  const messages = motivationalMessages.indonesia;
+  const todaysMessage = messages[treatmentDay.toString()];
+
+  if(!todaysMessage) return "";
+
+  return (<p data-testid='motivational-message' >{todaysMessage || treatmentDay}</p>)
+
+}
 
 export default Greeting;
