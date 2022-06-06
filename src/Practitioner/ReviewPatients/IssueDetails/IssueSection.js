6161baf3009ad28430e447c06ebacca6732ad70d
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-06-06 16:37:35 -0400

    Animation for collapsing icon working

--- src/Practitioner/ReviewPatients/IssueDetails/IssueSection.js
+++ src/Practitioner/ReviewPatients/IssueDetails/IssueSection.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Grid, Hidden } from '@material-ui/core';
+import { Box, Collapse, Grid, Hidden, IconButton } from '@material-ui/core';
 import Colors from '../../../Basics/Colors';
 import SubSectionTitle from '../../../Components/Practitioner/SubSectionTitle';
+import { KeyboardArrowDown } from '@material-ui/icons';
+import useToggle from '../../../Hooks/useToggle';
+import ExpansionToggle from '../../../Components/ExpansionToggle';
 
 const useStyles = makeStyles({
     sectionHeader: {
+        backgroundColor: "#EEEEEE",
+        padding: "8px",
+        borderRadius: "4px 4px 0 0",
         "& svg": {
             fontSize: "1.5em",
             color: Colors.textGray
@@ -13,35 +19,42 @@ const useStyles = makeStyles({
     },
     icon: {
         color: "black"
+    },
+    toggleButton:{
+        padding: "0"
     }
 })
 
-const SectionTitle = ({ icon, title, number }) => {
+const SectionTitle = ({ icon, title, number, showDetails, toggleDetails }) => {
 
     const classes = useStyles();
 
-    return (
-        <Box padding=".5em .25em" >
-            <Grid className={classes.sectionHeader} wrap="nowrap" alignItems="center" container>
-                {icon && React.createElement(icon, { style: { color: "black" } })}
-                {icon && <Box width=".5em" />}
-                {title && <SubSectionTitle>{title}: {number}</SubSectionTitle>}
-            </Grid>
-        </Box>)
+    return (<Grid className={classes.sectionHeader} wrap="nowrap" alignItems="center" container>
+        {icon && React.createElement(icon, { style: { color: "black" } })}
+        {icon && <Box width=".5em" />}
+        {title && <SubSectionTitle>{title}: {number}</SubSectionTitle>}
+        <IconButton onClick={toggleDetails} className={classes.toggleButton}>
+            <ExpansionToggle expanded={showDetails} />
+        </IconButton>
+    </Grid>)
 }
 
 const IssueSection = (props) => {
+
+    const [showDetails, toggleDetails] = useToggle(true);
 
     return (
         <>
             <Grid xs={12} md={"auto"}>
                 <Box marginBottom="1em">
-                    {(props.icon || props.title) && <SectionTitle {...props} />}
-                    <Box borderRadius="4px" border={`solid 1px ${Colors.lightgray}`} padding="0 .5em">
-                        <Box padding=".5em" borderRadius="0 0 4px 4px" >
-                            {props.children}
+                    {(props.icon || props.title) && <SectionTitle showDetails={showDetails} toggleDetails={toggleDetails} {...props} />}
+                    <Collapse in={showDetails}>
+                        <Box borderRadius="4px" border={`solid 1px ${Colors.lightgray}`} padding="0 .5em">
+                            <Box padding=".5em" borderRadius="0 0 4px 4px" >
+                                {props.children}
+                            </Box>
                         </Box>
-                    </Box>
+                    </Collapse>
                 </Box>
             </Grid>
             <Hidden smDown>
