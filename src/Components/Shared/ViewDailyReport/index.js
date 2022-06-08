56bfdcb997ff11311ec55d05bf04d8e9ba08bd1c
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-06-08 11:57:34 -0400

    Refactor status into shared component

--- src/Components/Shared/ViewDailyReport/index.js
+++ src/Components/Shared/ViewDailyReport/index.js
@@ -4,7 +4,7 @@ import PillIcon from '../../../Basics/Icons/Pill.js'
 import { useTranslation } from 'react-i18next';
 import { Box, Grid, Typography } from '@material-ui/core';
 import Symptom from '../../../Practitioner/Shared/Symptom';
-import { Assignment, SentimentDissatisfied } from '@material-ui/icons';
+import { Assignment, InsertEmoticon, SentimentVeryDissatisfied, SentimentVerySatisfied } from '@material-ui/icons';
 import ZoomableImage from '../ZoomableImage';
 import ExpandableCard from '../../ExpandableCard';
 
@@ -14,10 +14,11 @@ import Colors from '../../../Basics/Colors.js';
 import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
 
 const Status = ({ text, icon, color }) => {
+
     return (<Grid container >
         {React.createElement(icon, { style: { color: color } })}
         <Box width="8px" />
-        <Typography>
+        <Typography style={{ textTransform: "capitalize" }}>
             {text}
         </Typography>
     </Grid >)
@@ -42,13 +43,11 @@ const DoingOkay = ({ value }) => {
 
     const { t } = useTranslation('translation');
 
-    return <Grid container>
-        {value ? <CheckCircleIcon style={{ color: Colors.green }} /> : <HighlightOffIcon style={{ color: Colors.red }} />}
-        <Box width="8px" />
-        <Typography style={{ textTransform: "capitalize" }}>
-            {value ? t('patient.report.doingWell') : t('patient.report.needSupport')}
-        </Typography>
-    </Grid>
+    return (<Status
+        text={value ? t('commonWords.yes') : t('commonWords.no')}
+        icon={value ? CheckCircleIcon : HighlightOffIcon}
+        color={vale ? Colors.green : Colors.red}
+    />)
 }
 
 const DailyReport = ({ report, date }) => {
@@ -63,31 +62,38 @@ const DailyReport = ({ report, date }) => {
     return (<Box bgcolor="white" padding="0 1em">
 
         <ExpandableCard hideToggle title={t('patient.report.didYouTake')} icon={PillIcon}>
-            {/* <Typography style={{ fontStyle: "italic" }}>{t('patient.report.didYouTake')}</Typography> */}
-            <YesNo value={medicationWasTaken} />
+            <Status
+                text={medicationWasTaken ? t('commonWords.yes') : t('commonWords.no')}
+                icon={medicationWasTaken ? CheckCircleIcon : HighlightOffIcon}
+                color={medicationWasTaken ? Colors.green : Colors.red}
+            />
 
             {!medicationWasTaken &&
                 <>
                     <Seperator />
-                    <Typography style={{ fontStyle: "italic" }}>{t('patient.report.whyNotTaken')}</Typography>
-                    <Typography>{whyMedicationNotTaken || "No reason given"}</Typography>
+                    <Typography>{t('patient.report.whyNotTaken')}</Typography>
+                    <Typography style={{ fontStyle: "italic" }}>{whyMedicationNotTaken || t('coordinator.sideBar.noReason')}</Typography>
                 </>}
         </ExpandableCard>
 
         <ExpandableCard hideToggle title={t('patient.report.symptomsTitle')} icon={Assignment}>
-            <Grid container>
-                {!hadSymptoms ? <CheckCircleIcon style={{ color: Colors.green }} /> : <HighlightOffIcon style={{ color: Colors.red }} />}
-                <Box width="8px" />
-                <Typography>{hadSymptoms ? t('coordinator.sideBar.symptomsSince') : t('coordinator.recentReports.noSymptoms')}</Typography>
-            </Grid>
+            <Status
+                color={hadSymptoms ? Colors.red : Colors.green}
+                icon={!hadSymptoms ? CheckCircleIcon : HighlightOffIcon}
+                text={hadSymptoms ? t('coordinator.sideBar.symptomsSince') : t('coordinator.recentReports.noSymptoms')}
+            />
             {hadSymptoms && <>
                 <Seperator />
                 <SymptomList symptoms={symptoms} />
             </>}
         </ExpandableCard>
 
-        <ExpandableCard hideToggle title={t('patient.report.moodTitle')} icon={SentimentDissatisfied}>
-            <DoingOkay value={doingOkay} />
+        <ExpandableCard hideToggle title={t('patient.report.moodTitle')} icon={InsertEmoticon}>
+            <Status
+                text={doingOkay ? t('patient.report.doingWell') : t('patient.report.needSupport')}
+                icon={doingOkay ? SentimentVerySatisfied : SentimentVeryDissatisfied}
+                color={doingOkay ? Colors.green : Colors.red}
+            />
             {doingOkayReason && <Typography>{doingOkayReason}</Typography>}
         </ExpandableCard>
         <ExpandableCard hideToggle title={t('commonWords.stripPhoto')} icon={CameraIcon}>
