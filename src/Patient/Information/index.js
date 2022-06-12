90ba08f2440d21712dc39c461e59819b80b0c4e5
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-06-12 17:00:11 -0400

    Questions and answers section flushed out

--- src/Patient/Information/index.js
+++ src/Patient/Information/index.js
@@ -4,8 +4,11 @@ import { Route, Switch, Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import TopPageLabel from '../../Components/Shared/TopPageLabel';
 import { PageLabel } from '../../Components/Shared/PageLabel';
-import { VideoCallOutlined, YouTube } from '@material-ui/icons';
-import Colors from '../../Basics/Colors';
+import { AccessAlarm, AccessAlarmRounded, MenuBook, QuestionAnswerRounded, VideoCallOutlined, YouTube } from '@material-ui/icons';
+import HelpVideos from './HelpVideos';
+import { useTranslation } from 'react-i18next';
+import { t } from 'i18next';
+import QuestionsAndAnswers from './QuestionsAndAnswers';
 
 const useStyles = makeStyles({
     grid: {
@@ -32,28 +35,36 @@ const useStyles = makeStyles({
     },
     sectionTitle: {
         fontSize: "1.25rem"
+    },
+    buttonText: {
+        lineHeight: "1.1rem",
+        fontWeight: "500",
+        textTransform: "capitalize"
     }
 })
+
+const Translate = ({ children }) => {
+    const { t } = useTranslation();
+    return <>{t(children)}</>
+}
 
 
 const buttonData = [
     {
-        sectionTitle: "Preferences", items: [
-            { translationKey: "Medication Reminder", to: "/information/reminder", icon: <YouTube /> },
-            { translationKey: "Questions and Answers", to: "/information/faq" }
+        sectionTitle: 'patient.profile.title', items: [
+            { translationKey: 'patient.profile.options.medicationReminder', to: "/information/reminder", icon: <AccessAlarmRounded /> },
         ]
     },
     {
-        sectionTitle: "Information about treatment", items: [
-            { translationKey: "Videos", to: "/information/videos", icon: <YouTube /> },
-            { translationKey: "Questions and Answers", to: "/information/faq" }
+        sectionTitle: 'patient.information.infoSection', items: [
+            { translationKey: 'patient.information.questions', to: "/information/faq", icon: <QuestionAnswerRounded />, page: <QuestionsAndAnswers /> },
+            { translationKey: 'patient.information.videos', to: "/information/videos", icon: <YouTube /> },
         ]
     },
     {
-        sectionTitle: "Help with app",
+        sectionTitle: 'patient.information.helpSection',
         items: [
-            { translationKey: "Help Videos", to: "/information/help-videos" },
-            { translationKey: "Report an Issue", to: "/information/issue" },
+            { translationKey: "Help Videos", to: "/information/help-videos", icon: <YouTube />, page: <HelpVideos /> },
         ]
     }
 
@@ -66,7 +77,7 @@ const InfoRoute = (props) => {
 
     return <Route {...props}>
         <Box width="100%" bgcolor="white" zIndex={10} position="fixed" top={0}>
-            <PageLabel title={title} />
+            <PageLabel title={<Translate>{title}</Translate>} />
         </Box>
         {children}
     </Route>
@@ -83,9 +94,9 @@ export default function InformationPage() {
     return (
         <Switch>
             {buttons.map(_each => {
-                const { to, translationKey } = _each;
+                const { to, translationKey, page } = _each;
                 return <InfoRoute key={`route-${to}`} title={translationKey} path={to}>
-                    <p>Content!</p>
+                    {page || <p>Content missing, please add it</p>}
                 </InfoRoute>
             })}
             <Route>
@@ -108,21 +119,30 @@ const Buttons = () => {
         <Box padding="0 16px">
             {buttonData.map(each => {
                 return <React.Fragment key={each.sectionTitle}>
-                    <SectionTitle>{each.sectionTitle}</SectionTitle>
+                    <SectionTitle>
+                        <Translate>
+                            {each.sectionTitle}
+                        </Translate>
+                    </SectionTitle>
                     <div className={classes.grid}>
                         {each.items.map(_each => {
                             const { icon, translationKey, to } = _each;
                             return <Paper elevation={2} key={`button-${to}`}>
                                 <ButtonBase className={classes.box} component={Link} to={to}>
                                     {icon}
-                                    <Typography>{translationKey}</Typography>
+                                    <Box height="8px" />
+                                    <Typography className={classes.buttonText}>
+                                        <Translate>
+                                            {translationKey}
+                                        </Translate>
+                                    </Typography>
                                 </ButtonBase>
                             </Paper>
                         })}
                     </div>
                 </React.Fragment>
-
             })}
+            <Box height="60px" />
         </Box >
     )
 }
