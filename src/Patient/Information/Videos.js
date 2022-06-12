c7367ca9490195c9e25d9059582795138b55ef1f
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-06-12 18:07:37 -0400

    Move video preview images to public folder

--- src/Patient/Information/Videos.js
+++ src/Patient/Information/Videos.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import makeStyles from '@material-ui/styles/makeStyles';
-import PreviewOne from './ministry-of-health-video.jpg';
-import PreviewTwo from './province-of-misiones-video.png';
 import { Box, Button } from '@material-ui/core';
 import Colors from '../../Basics/Colors'
 import { useTranslation } from 'react-i18next';
@@ -40,12 +38,12 @@ export default function Videos() {
     return (
                 <Box padding="2rem 1rem">
                     <div className={classes.video}>
-                        <a href="https://www.youtube.com/watch?v=KizqF_HmI2w"><img className={classes.preview} src={PreviewOne}></img></a>
+                        <a href="https://www.youtube.com/watch?v=KizqF_HmI2w"><img className={classes.preview} src={"/img/es-Ar/video-previews/ministry-of-health-video.jpg"}></img></a>
                         <Button className={classes.button} href="https://www.youtube.com/watch?v=KizqF_HmI2w">
                             Video de Ministerio de Salud</Button>
                     </div>
                     <div className={classes.video}>
-                        <a href="https://www.youtube.com/watch?v=vaXrKW0ZGtg"><img className={classes.preview} src={PreviewTwo}></img></a>
+                        <a href="https://www.youtube.com/watch?v=vaXrKW0ZGtg"><img className={classes.preview} src={"/img/es-Ar/video-previews/province-of-misiones-video.png"}></img></a>
                         <Button className={classes.button} href="https://www.youtube.com/watch?v=vaXrKW0ZGtg">
                             Programa Tuberculosis Provincia de Misiones</Button>
                     </div>
