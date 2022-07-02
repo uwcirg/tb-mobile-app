100e3f3f2055f49d00e86cd847fccdc31a4196e6
johnny2smooth <johnbp13@gmail.com>
2022-07-02 11:32:18 -0700

    instructions swipeable

--- src/Patient/Information/TestInstructions/index.js
+++ src/Patient/Information/TestInstructions/index.js
@@ -1,12 +1,19 @@
 import React, { useReducer } from 'react';
 import InstructionStep from './InstructionStep';
-import { Grid, Box, makeStyles, MobileStepper, IconButton } from '@material-ui/core';
+import {
+  Grid,
+  Box,
+  makeStyles,
+  MobileStepper,
+  IconButton,
+} from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import ChevronLeft from '@material-ui/icons/ChevronLeft';
 import ChevronRight from '@material-ui/icons/ChevronRight';
 import Colors from '../../../Basics/Colors';
 import { PageLabel } from '../../../Components/Shared/PageLabel';
 import { withStyles } from '@material-ui/styles';
+import { useSwipeable } from 'react-swipeable';
 
 function reducer(state, action) {
   switch (action.type) {
@@ -32,54 +39,78 @@ const useStyles = makeStyles({
   },
   step: { maxWidth: '400px' },
   container: {
-    boxSizing: "border-box",
-    minHeight: "calc(90vh - 60px)",
-    padding: "1em"
-  }
+    boxSizing: 'border-box',
+    minHeight: 'calc(90vh - 60px)',
+    padding: '1em',
+  },
 });
 
 const TestInstructions = () => {
   const classes = useStyles();
   const { t } = useTranslation('translation');
 
+  const swipeHandlers = useSwipeable({
+    onSwiped: (eventData) => {
+      if (step < 5 && eventData.dir === 'Left') {
+        dispatch({ type: 'next' });
+      }
+      if (step > 0 && eventData.dir === 'Right') {
+        dispatch({ type: 'previous' });
+      }
+    },
+  });
+
   const [step, dispatch] = useReducer(reducer, 0);
 
   return (
-      <Grid className={classes.container} direction='column' container>
-         <Grid justify='space-between' container>
-          <StepperButton
-            children={<ChevronLeft />}
-            onClick={() => { dispatch({ type: 'previous' }) }}
-            disabled={step === 0} />
-          <MobileStepper
-            style={{ backgroundColor: "white" }}
-            steps={6}
-            variant="dots"
-            position="static"
-            activeStep={step} />
-          <StepperButton
-            children={<ChevronRight />}
-            onClick={() => { dispatch({ type: 'next' }) }}
-            disabled={step >= 5}
-          />
-        </Grid>
-        <Box style={{ boxSizing: "border-box" }} flexGrow="1" padding="0 1em" width="100%" maxWidth="500px">
+    <Grid className={classes.container} direction="column" container>
+      <Grid justify="space-between" container>
+        <StepperButton
+          children={<ChevronLeft />}
+          onClick={() => {
+            dispatch({ type: 'previous' });
+          }}
+          disabled={step === 0}
+        />
+        <MobileStepper
+          style={{ backgroundColor: 'white' }}
+          steps={6}
+          variant="dots"
+          position="static"
+          activeStep={step}
+        />
+        <StepperButton
+          children={<ChevronRight />}
+          onClick={() => {
+            dispatch({ type: 'next' });
+          }}
+          disabled={step >= 5}
+        />
+      </Grid>
+      <Box
+        style={{ boxSizing: 'border-box' }}
+        flexGrow="1"
+        padding="0 1em"
+        width="100%"
+        maxWidth="500px"
+      >
+        <div {...swipeHandlers}>
           <InstructionStep className="step" currentStep={step} />
-
-        </Box>
-      </Grid >
+        </div>
+      </Box>
+    </Grid>
   );
 };
 
 const StepperButton = withStyles({
   root: {
-    "&:focus, &:hover": {
-      backgroundColor: Colors.buttonBlue
+    '&:focus, &:hover': {
+      backgroundColor: Colors.buttonBlue,
     },
-    borderRadius: "4px",
+    borderRadius: '4px',
     backgroundColor: Colors.buttonBlue,
-    color: "white"
+    color: 'white',
   },
-})(IconButton)
+})(IconButton);
 
 export default TestInstructions;
