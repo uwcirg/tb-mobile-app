5c069bde89f43fe4c9decef5c0a43b79be58ac93
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-07-13 10:50:11 -0700

    Fix styling of nex appointment card

--- src/Components/Shared/Appointments/ViewAppointments/index.js
+++ src/Components/Shared/Appointments/ViewAppointments/index.js
@@ -5,17 +5,17 @@ import { Box, Typography } from "@material-ui/core";
 import ClickableText from "../../../../Basics/ClickableText";
 import SharedAPI from "../../../../API/SharedAPI";
 import useAsyncWithParams from "../../../../Hooks/useAsyncWithParams";
-import useToggle from "../../../../Hooks/useToggle";
 import PropTypes from "prop-types";
 import Colors from "../../../../Basics/Colors";
-import ExpansionToggle from "../../../ExpansionToggle";
 import { useHistory } from "react-router-dom";
 import { KeyboardArrowRight } from "@material-ui/icons";
 
 export default function ViewAppointments({ patientId }) {
   const { t } = useTranslation("translation");
 
-  const { value: appointments } = useAsyncWithParams({
+  const history = useHistory();
+
+  const { value: apts } = useAsyncWithParams({
     asyncFunc: SharedAPI.getAppointments,
     immediate: true,
     funcParams: [patientId],
@@ -24,10 +24,22 @@ export default function ViewAppointments({ patientId }) {
 
   return (
     <Box width="100%" padding="0 .5rem" style={{ boxSizing: "border-box" }}>
-      <AppointmentList
-        appointments={appointments}
-        header={t(`patient.progress.upcoming`)}
-      />
+      <Box width="100%">
+        <Title />
+        <Box height="16px" />
+        {apts && apts[0] && <ReminderLineItem reminder={apts[0]} />}
+        <ClickableText
+          hideIcon
+          text={
+            <>
+              {t("appointments.manage")} <KeyboardArrowRight />
+            </>
+          }
+          onClick={() => {
+            history.push("/information/appointments");
+          }}
+        />
+      </Box>
     </Box>
   );
 }
@@ -36,42 +48,7 @@ const Title = () => {
   const { t } = useTranslation("translation");
   return (
     <Box width="100%" paddingBottom="8px" borderBottom="solid 1px lightgray">
-      <Typography
-        style={{ fontSize: "1.2rem", color: Colors.textDarkGray }}
-        variant="h1"
-      >
-        {t("appointments.nextAppointment")}
-      </Typography>
-    </Box>
-  );
-};
-
-const AppointmentList = ({ appointments: apts }) => {
-  const { t } = useTranslation("translation");
-
-  const history = useHistory();
-
-  return (
-    <Box width="100%">
-      <Title />
-      <Box height="16px" />
-      {apts &&
-        apts
-          .slice(0, 1)
-          .map((each) => (
-            <ReminderLineItem key={`reminder-${each.id}`} reminder={each} />
-          ))}
-      <ClickableText
-        hideIcon
-        text={
-          <>
-            View / Add Appointments <KeyboardArrowRight />
-          </>
-        }
-        onClick={() => {
-          history.push("/information/appointments");
-        }}
-      />
+      <Typography>{t("appointments.nextAppointment")}</Typography>
     </Box>
   );
 };
