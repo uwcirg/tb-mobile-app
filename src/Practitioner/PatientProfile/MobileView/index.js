4324ff4da9455b9c308b430bb08edbaaede994b5
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-07-15 11:02:44 -0400

    Extend ClickableText shared component to use react-router link

--- src/Practitioner/PatientProfile/MobileView/index.js
+++ src/Practitioner/PatientProfile/MobileView/index.js
@@ -2,18 +2,13 @@ import React from "react";
 import useStores from "../../../Basics/UseStores";
 import { observer } from "mobx-react";
 import StickyTopBar from "../../../Components/Shared/StickyTopBar";
-import { Box, Grid, Typography } from "@material-ui/core";
+import { Box } from "@material-ui/core";
 import Colors from "../../../Basics/Colors";
-import Priority from "../../Shared/Priority";
 import ButtonList from "../Buttons";
-import Label from "../../../Components/Label";
 import { PageLabel } from "../../../Components/Shared/PageLabel";
-import { DateTime } from "luxon";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import ReportingPopOver from "../../Shared/ReportingPopOver";
 import { useHistory } from "react-router-dom";
-import ReportingHistoryLinks from "../../../Components/Shared/ReportingHistoryLinks";
-import SectionTitle from "./SectionTitle";
 import PhotoAdherence from "../Adherence/Photo";
 import MedicationAdherence from "../Adherence/Medication";
 import { useTranslation } from "react-i18next";
@@ -73,35 +68,5 @@ const MobileView = observer(() => {
   );
 });
 
-/*
-adherence: (...)
-age: (...)
-appStartTime: (...)
-channelId: (...)
-contactTracingSurvey: (...)
-daysInTreatment: (...)
-familyName: (...)
-fullName: (...)
-gender: (...)
-givenName: (...)
-id: (...)
-lastContacted: (...)
-lastMissedDay: (...)
-lastReport: (...)
-lastSymptoms: (...)
-medicationSummary: (...)
-organizationId: (...)
-percentageComplete: (...)
-phoneNumber: (...)
-photoAdherence: (...)
-photoSummary: (...)
-priority: (...)
-reportingStatus: (...)
-status: (...)
-supportRequests: (...)
-treatmentEndDate: (...)
-treatmentStart: (...)
-weeksInTreatment: (...)
-*/
 
 export default MobilePatientProfile;
