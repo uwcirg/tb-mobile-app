23dc226f4deae2b3ff7391aacb9b9de3f3075853
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-07-18 10:04:36 -0400

    Use specific md file based on current locale

--- src/Patient/Information/QuestionsAndAnswers.js
+++ src/Patient/Information/QuestionsAndAnswers.js
@@ -1,15 +1,23 @@
-import { Box } from '@material-ui/core'
-import React from 'react'
+import { Box } from "@material-ui/core";
+import React from "react";
 
-import Markdown from 'markdown-to-jsx';
+import Markdown from "markdown-to-jsx";
 import raw from "raw.macro";
-import MarkdownRender from './Panel';
+import MarkdownRender from "./Panel";
+import { useTranslation } from "react-i18next";
 
-const es = raw('../../Content/faq.md');
-const indonesiaa = raw('../../Content/faq-id.md');
+const es = raw("../../Content/faq.md");
+const id = raw("../../Content/faq-id.md");
 
 export default function QuestionsAndAnswers() {
-    return (<Box>
-        <Markdown options={{ overrides: { Drawer: { component: MarkdownRender } } }} children={indonesiaa} />
-    </Box>)
+  const { i18n } = useTranslation();
+
+  return (
+    <Box>
+      <Markdown
+        options={{ overrides: { Drawer: { component: MarkdownRender } } }}
+        children={i18n.language === "id" ? id : es}
+      />
+    </Box>
+  );
 }
