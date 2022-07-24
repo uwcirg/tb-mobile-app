c6878dcf49765e95c1391df23a395535fc4e04e9
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-07-24 16:32:01 -0400

    Refactor messy grid implementation

--- src/Login/PapuaLogin/Form.js
+++ src/Login/PapuaLogin/Form.js
@@ -7,57 +7,63 @@ import { useTranslation } from "react-i18next";
 import Colors from "../../Basics/Colors";
 import Loading from "../../Practitioner/Shared/Loading";
 
-const Form = observer(({ isProvider }) => {
+const Form = observer(({ isProvider, isActivation }) => {
   const { loginStore } = useStores();
   const { t } = useTranslation();
 
   return (
-    <Grid container spacing={1}>
-      <Grid xs={12} item>
-        <Input
-          onChange={(e) => {
-            loginStore.setIdentifier(e.target.value);
-          }}
-          value={loginStore.identifier}
-          autoComplete="username"
-          InputProps={!isProvider ? {inputMode: 'numeric'} : {}}
-          placeholder={
-            isProvider
-              ? t("login.email").toLowerCase()
-              : t("login.phoneNumber").toLowerCase()
-          }
-        />
-      </Grid>
-      <Grid xs={12} item>
-        <Input
-          autoComplete="password"
-          onChange={(e) => {
-            loginStore.setPassword(e.target.value);
-          }}
-          value={loginStore.password}
-          type="password"
-          placeholder={t("login.password").toLowerCase()}
-        />
-      </Grid>
-      <Grid xs={12} item>
-        <Button
-          onClick={loginStore.submitCombinedLogin}
-          style={{
-            borderRadius: "18px",
-            backgroundColor: Colors.papuaGreen,
-            color: "white",
-            minHeight: "45px",
-          }}
-          fullWidth
-          disableElevation
-          variant="contained"
-        >
-          {t("login.logIn")}
-        </Button>
-        {loginStore.loading && <Loading />}
-      </Grid>
-    </Grid>
+    <WrappedGrid>
+      <Input
+        onChange={(e) => {
+          loginStore.setIdentifier(e.target.value);
+        }}
+        value={loginStore.identifier}
+        autoComplete="username"
+        InputProps={!isProvider ? { inputMode: "numeric" } : {}}
+        placeholder={
+          isProvider
+            ? t("login.email").toLowerCase()
+            : t("login.phoneNumber").toLowerCase()
+        }
+      />
+      <Input
+        autoComplete="password"
+        onChange={(e) => {
+          loginStore.setPassword(e.target.value);
+        }}
+        value={loginStore.password}
+        type="password"
+        placeholder={t("login.password").toLowerCase()}
+      />
+      <Button
+        onClick={loginStore.submitCombinedLogin}
+        style={{
+          borderRadius: "18px",
+          backgroundColor: Colors.papuaGreen,
+          color: "white",
+          minHeight: "45px",
+        }}
+        fullWidth
+        disableElevation
+        variant="contained"
+      >
+        {t("login.logIn")}
+      </Button>
+      {loginStore.loading && <Loading />}
+    </WrappedGrid>
   );
 });
+
+const WrappedGrid = ({ children }) => {
+  return (
+    <Grid container spacing={1}>
+      {children.map((child) => (
+        <Grid item xs={12}>
+          {child}
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
 
 export default Form;
