389100ca934b85f19054469698dda907496cfa23
Kyle Goodwin <kjgoodwins@gmail.com>
2022-07-26 11:28:49 -0400

    Fix/photo reports view v2 (#443)
    * Fix shortdate parsing adding months to parsed date
    
    * Implement component to display photo coding status
    
    * Refactor out details for each photo day from list
    
    * Improve message for missing day on photo reports list
    
    Co-authored-by: Kyle Goodwin <kylegoodwin@users.noreply.github.com>

--- src/Components/Shared/ReportViews/PhotoList/index.js
+++ src/Components/Shared/ReportViews/PhotoList/index.js
@@ -1,29 +1,36 @@
-import { Box, Button } from '@material-ui/core';
-import { DateTime } from 'luxon';
-import React, { useState } from 'react';
-import ShortDate from '../../ShortDate';
-import ReportContainer from '../ReportContainer';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { Box, Button } from "@material-ui/core";
+import { DateTime } from "luxon";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import SingleDay from "./SingleDay";
 
 export default function PhotoList({ photoDays, reportsHash }) {
+  const { t } = useTranslation("translation");
 
-    const { t } = useTranslation('translation');
+  const [days, setDays] = useState(7);
+  const photoDaysToDisplay = photoDays
+    .filter((each) => {
+      return DateTime.fromISO(each).diffNow("days").days < 0;
+    })
+    .reverse();
+  const toShow = photoDaysToDisplay.slice(0, days);
 
-    
-    const [days, setDays] = useState(7);
-    const photoDaysToDisplay = photoDays.filter(each => { return DateTime.fromISO(each).diffNow('days').days < 0 }).reverse()
-    const toShow = photoDaysToDisplay.slice(0, days)
-
-    return (
-        <Box padding="16px">
-            {toShow.map(date => {
-                return <ReportContainer key={`photo-preview-${date}`} to={`?date=${date}`} disabled={!reportsHash[date]}>
-                    <Box padding="8px">
-                        <ShortDate date={date} />
-                    </Box>
-                </ReportContainer>
-            })}
-            <Button onClick={() => { setDays(days + 7) }}>{t('commonWords.loadMore')}</Button>
-        </Box>
-    )
+  return (
+    <Box padding="16px">
+      {toShow.map((date) => {
+        const report = reportsHash[date];
+        return (
+          <SingleDay key={`photo-list-${date}`} date={date} report={report} />
+        );
+      })}
+      <Button
+        onClick={() => {
+          setDays(days + 7);
+        }}
+      >
+        {t("commonWords.loadMore")}
+      </Button>
+    </Box>
+  );
 }
