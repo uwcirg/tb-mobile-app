778ebf100d68b99130cccc0faf7c393d37474626
johnny2smooth <johnbp13@gmail.com>
2022-08-30 07:28:03 -0700

    overlay now flexible to all screen sizes

--- src/ImageCapture/ImageOverlay.js
+++ src/ImageCapture/ImageOverlay.js
@@ -6,27 +6,28 @@ export default function ImageOverlay() {
     <div
       style={{
         backgroundColor: 'white',
-        position: 'relative',
+        position: 'absolute',
         width: '90%',
-        height: '95%',
-        opacity: 0.6,
+        height: '90%',
+        opacity: 0.4,
         zIndex: '1',
         borderRadius: '5px',
         margin: '1em auto',
+        left: '1em',
+        top: '1em',
       }}
     >
-      <p>Please keep your camera steady until the process is over</p>
       <img
         src={overlay}
         alt="overlay is going to go right here"
         style={{
-          height: '400px',
+          height: '75vh',
           maxWidth: '100%',
           margin: '1em auto',
           backgroundColor: 'transparent',
           position: 'relative',
           opacity: 0.9,
-          zIndex: '1',
+          zIndex: '2',
         }}
       />
     </div>
