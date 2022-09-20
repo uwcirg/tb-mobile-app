91120d7964c38e5c440fec8d1ada4a25c2371bad
johnny2smooth <johnbp13@gmail.com>
2022-09-20 17:22:11 -0400

    all cards added

--- src/Components/Shared/ReportingHistoryLinks.js
+++ src/Components/Shared/ReportingHistoryLinks.js
@@ -1,47 +1,68 @@
 import { Box, ButtonBase, Grid, Typography } from '@material-ui/core';
-import { CameraAlt, ListAlt, Event } from '@material-ui/icons';
+import { CameraAlt, ListAlt, Event, ViewList } from '@material-ui/icons';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Colors from '../../Basics/Colors';
+import IssueCard from '../../Practitioner/ReviewPatients/IssueDetails/IssueCard';
 
 const useStyles = makeStyles({
-    button: {
-        width: "60px",
-        color: Colors.buttonBlue,
-        flexDirection: "column",
-        justifyContent: 'center'
-    },
-    label: {
-        fontSize: ".75em",
-        textTransform: "capitalize",
-        color: Colors.textDarkGray
-    }
-})
+  button: {
+    width: '60px',
+    color: Colors.buttonBlue,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  label: {
+    fontSize: '.75em',
+    textTransform: 'capitalize',
+    color: Colors.textDarkGray,
+  },
+});
 
 const ReportingHistoryLinks = ({ patient }) => {
+  const { t } = useTranslation('translation');
 
-    const { t } = useTranslation('translation');
-
-    return (<Box flexGrow={1} borderRadius="5px" bgcolor={Colors.lighterGray} padding="8px">
-        <Grid alignItems='center' container justify='space-between'>
-            <SingleButton to={`${patient.id}/reports/calendar`} icon={<Event />} text={t('patient.tabNames.calendar')} />
-            <SingleButton to={`${patient.id}/reports/list`} icon={<ListAlt />} text={t('commonWords.list')} />
-            <SingleButton to={`${patient.id}/reports/photos`} icon={<CameraAlt />} text={t('commonWords.photos')} />
+  return (
+    <IssueCard title={t('commonWords.reportingSummary')} icon={<ViewList />}>
+      <Box
+        flexGrow={1}
+        borderRadius="5px"
+        bgcolor={Colors.lighterGray}
+        padding="8px"
+      >
+        <Grid alignItems="center" container justify="space-between">
+          <SingleButton
+            to={`${patient.id}/reports/calendar`}
+            icon={<Event />}
+            text={t('patient.tabNames.calendar')}
+          />
+          <SingleButton
+            to={`${patient.id}/reports/list`}
+            icon={<ListAlt />}
+            text={t('commonWords.list')}
+          />
+          <SingleButton
+            to={`${patient.id}/reports/photos`}
+            icon={<CameraAlt />}
+            text={t('commonWords.photos')}
+          />
         </Grid>
-    </Box>)
-
-}
+      </Box>
+    </IssueCard>
+  );
+};
 
 const SingleButton = ({ icon, text, to }) => {
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    return (<ButtonBase className={classes.button} component={Link} to={to}>
-        {icon}
-        <Typography className={classes.label}>{text}</Typography>
-    </ButtonBase>)
-}
+  return (
+    <ButtonBase className={classes.button} component={Link} to={to}>
+      {icon}
+      <Typography className={classes.label}>{text}</Typography>
+    </ButtonBase>
+  );
+};
 
 export default ReportingHistoryLinks;
