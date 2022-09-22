e9a22c9756f205bec0758c62e2ff61163e4bd102
johnny2smooth <johnbp13@gmail.com>
2022-09-22 10:46:31 -0400

    patient profile store now allows edits to treatment start date. change patient detials component has datetime picker for edits made to treatment start date

--- src/Practitioner/PatientProfile/Dialogs/ChangePatientDetails.js
+++ src/Practitioner/PatientProfile/Dialogs/ChangePatientDetails.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import useStores from '../../../Basics/UseStores';
-import { observer } from 'mobx-react'
+import { observer } from 'mobx-react';
 import PopOver from '../../Shared/PopOver';
-import makeStyles from '@material-ui/styles/makeStyles'
-import TextField from '@material-ui/core/TextField'
+import makeStyles from '@material-ui/styles/makeStyles';
+import TextField from '@material-ui/core/TextField';
 import { useTranslation } from 'react-i18next';
 import Button from '@material-ui/core/Button';
 import Colors from '../../../Basics/Colors';
@@ -11,137 +11,177 @@ import WarningBox from '../../../Basics/WarningBox';
 import DatePicker from '../../../Basics/DatePicker';
 import { DateTime } from 'luxon';
 
-const useStyles = makeStyles({
-    textInput: {
-        "& > div > input": {
-            padding: "10px"
-        }
-    },
-    inputs: {
-        width: "70%",
-    },
-    label: {
-        display: "block",
-        marginBottom: "5px"
-    },
+const ChangePatientDetails = observer(({ handleClose }) => {
+  const { t } = useTranslation('translation');
+  const classes = useStyles();
+  const { patientProfileStore } = useStores();
 
-    popOverBody: {
-        marginBottom: "1em"
-    },
-    formControl: {
-        marginTop: "2em",
-        width: "100%",
-        display: "flex",
-        justifyContent: "flex-end",
-        "& > button": {
-            margin: "0 .25em",
-            color: "white"
-        },
-        "& > button > span": {
-            textTransform: "capitalize"
-        },
-        "& > #cancel": {
-            backgroundColor: Colors.warningRed
-        },
-        "& > #submit": {
-            backgroundColor: Colors.buttonBlue,
-            "&:disabled": {
-                backgroundColor: "lightgray"
-            }
+  useEffect(() => {
+    patientProfileStore.initalizeChanges();
+  }, [patientProfileStore.selectedPatient.details]);
 
-        }
-    },
-    inputItem: {
-        margin: ".5em 0"
-    },
-    warning: {
-        marginTop: ".5em"
-    },
-    calendarInput: {
-        "& > input": {
-            padding: "10px"
-        }
-    }
-})
-
-const ChangePatientDetails = observer(({handleClose}) => {
-    const { t } = useTranslation('translation');
-    const classes = useStyles();
-    const { patientProfileStore } = useStores();
-
-    useEffect(() => {
-        patientProfileStore.initalizeChanges();
-    }, [patientProfileStore.selectedPatient.details])
-
-    return (
-        <PopOver ignoreClickAway close={handleClose} title={t('coordinator.patientProfile.editDetails.title')}>
-            <form className={classes.form}>
-                <p>
-                    {t('coordinator.patientProfile.editDetails.warning')}
-                </p>
-                <div className={classes.inputs}>
-                    <InputItem
-                        labelText={t('patient.userFields.firstName')}
-                        id="givenName"
-
-                    />
-                    <InputItem
-                        labelText={t('patient.userFields.lastName')}
-                        id="familyName"
-
-                    />
-                    <InputItem
-                        labelText={t('coordinator.patientProfile.phoneNumber')}
-                        id="phoneNumber"
-                    />
-                    <InputItem
-                        isDate
-                        labelText={t('coordinator.patientProfile.treatmentEnd')}
-                        id="treatmentEndDate"
-
-                    />
-                </div>
-                {patientProfileStore.hasErrorWithChanges && <WarningBox>{t('coordinator.patientProfile.editDetails.inputError')}</WarningBox>}
-                <div className={classes.formControl}>
-                    <Button disableElevation onClick={patientProfileStore.toggleOnChangeDetails} id="cancel" variant="contained" >{t('coordinator.patientProfile.editDetails.cancel')}</Button>
-                    <Button className={classes.submit} disabled={!patientProfileStore.hasChanges} onClick={patientProfileStore.postPatientChanges} disableElevation id="submit" variant="contained" >{t('coordinator.patientProfile.editDetails.submit')}</Button>
-                </div>
-
-            </form>
-        </PopOver>)
+  return (
+    <PopOver
+      ignoreClickAway
+      close={handleClose}
+      title={t('coordinator.patientProfile.editDetails.title')}
+    >
+      <form className={classes.form}>
+        <p>{t('coordinator.patientProfile.editDetails.warning')}</p>
+        <div className={classes.inputs}>
+          <InputItem
+            labelText={t('patient.userFields.firstName')}
+            id="givenName"
+          />
+          <InputItem
+            labelText={t('patient.userFields.lastName')}
+            id="familyName"
+          />
+          <InputItem
+            labelText={t('coordinator.patientProfile.phoneNumber')}
+            id="phoneNumber"
+          />
+          <InputItem
+            isDate
+            labelText={t('coordinator.patientProfile.treatmentEnd')}
+            id="treatmentEndDate"
+            editEndDate
+          />
+          <InputItem
+            isDate
+            labelText={t('patient.userFields.treatmentStart')}
+            id="treatmentStart"
+          />
+        </div>
+        {patientProfileStore.hasErrorWithChanges && (
+          <WarningBox>
+            {t('coordinator.patientProfile.editDetails.inputError')}
+          </WarningBox>
+        )}
+        <div className={classes.formControl}>
+          <Button
+            disableElevation
+            onClick={patientProfileStore.toggleOnChangeDetails}
+            id="cancel"
+            variant="contained"
+          >
+            {t('coordinator.patientProfile.editDetails.cancel')}
+          </Button>
+          <Button
+            className={classes.submit}
+            disabled={!patientProfileStore.hasChanges}
+            onClick={patientProfileStore.postPatientChanges}
+            disableElevation
+            id="submit"
+            variant="contained"
+          >
+            {t('coordinator.patientProfile.editDetails.submit')}
+          </Button>
+        </div>
+      </form>
+    </PopOver>
+  );
 });
 
 const InputItem = observer((props) => {
-    const classes = useStyles();
-    const { patientProfileStore } = useStores();
-    const error = patientProfileStore.changes.errors[props.id]
+  const classes = useStyles();
+  const { patientProfileStore } = useStores();
+  const error = patientProfileStore.changes.errors[props.id];
 
-    const handleDateChange = (date) => {
-        patientProfileStore.changeTreatmentEndDate(date.toISODate())
-    }
+  const editEndDate = (date) => {
+    patientProfileStore.changeTreatmentEndDate(date.toISODate());
+  };
 
-    return (
-        <div className={classes.inputItem}>
-            <label className={classes.label} htmlFor={props.id}>{props.labelText}</label>
-            {!props.isDate ? <TextField
-                className={classes.textInput}
-                id={props.id}
-                variant="outlined"
-                value={patientProfileStore.changes[props.id] || ""}
-                fullWidth
-                error={error}
-                onChange={e => {
-                    patientProfileStore.changes[props.id] = e.target.value;
-                }}
-            /> : <DatePicker
-                inputVariant="outlined"
-                InputProps={{ className: classes.calendarInput, fullWidth: true, }}
-                value={DateTime.fromISO(patientProfileStore.changes.treatmentEndDate)}
-                animateYearScrolling
-                onChange={handleDateChange}
-            />}
-        </div>
-    )
+  const editStartDate = (date) => {
+    patientProfileStore.changeTreatmentStart(date.toISODate());
+  };
+
+  console.log(props.id, patientProfileStore.changes[props.id]);
+
+  return (
+    <div className={classes.inputItem}>
+      <label className={classes.label} htmlFor={props.id}>
+        {props.labelText}
+      </label>
+      {!props.isDate ? (
+        <TextField
+          className={classes.textInput}
+          id={props.id}
+          variant="outlined"
+          value={patientProfileStore.changes[props.id] || ''}
+          fullWidth
+          error={error}
+          onChange={(e) => {
+            patientProfileStore.changes[props.id] = e.target.value;
+          }}
+        />
+      ) : (
+        <DatePicker
+          inputVariant="outlined"
+          InputProps={{ className: classes.calendarInput, fullWidth: true }}
+          value={
+            props.editEndDate
+              ? DateTime.fromISO(patientProfileStore.changes.treatmentEndDate)
+              : DateTime.fromISO(patientProfileStore.changes.treatmentStart)
+          }
+          animateYearScrolling
+          onChange={props.editEndDate ? editEndDate : editStartDate}
+        />
+      )}
+    </div>
+  );
+});
+
+const useStyles = makeStyles({
+  textInput: {
+    '& > div > input': {
+      padding: '10px',
+    },
+  },
+  inputs: {
+    width: '70%',
+  },
+  label: {
+    display: 'block',
+    marginBottom: '5px',
+  },
+
+  popOverBody: {
+    marginBottom: '1em',
+  },
+  formControl: {
+    marginTop: '2em',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    '& > button': {
+      margin: '0 .25em',
+      color: 'white',
+    },
+    '& > button > span': {
+      textTransform: 'capitalize',
+    },
+    '& > #cancel': {
+      backgroundColor: Colors.warningRed,
+    },
+    '& > #submit': {
+      backgroundColor: Colors.buttonBlue,
+      '&:disabled': {
+        backgroundColor: 'lightgray',
+      },
+    },
+  },
+  inputItem: {
+    margin: '.5em 0',
+  },
+  warning: {
+    marginTop: '.5em',
+  },
+  calendarInput: {
+    '& > input': {
+      padding: '10px',
+    },
+  },
 });
 
 export default ChangePatientDetails;
