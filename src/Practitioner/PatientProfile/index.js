db82ea4de54fbe3749bfb3644f22e98618e27796
johnny2smooth <johnbp13@gmail.com>
2022-10-03 07:27:21 -0700

    Patient appt list added to desktop view

--- src/Practitioner/PatientProfile/index.js
+++ src/Practitioner/PatientProfile/index.js
@@ -1,35 +1,37 @@
-import React, { useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import useStores from "../../Basics/UseStores";
-import { observer } from "mobx-react";
-import Styles from "../../Basics/Styles";
-import Colors from "../../Basics/Colors";
-import { useTranslation } from "react-i18next";
-import PatientInfo from "./Details/PatientInfo";
-import TreatmentStatus from "./Adherence/AdherenceSummary";
-import SymptomSummary from "./SymptomSummary";
-import TreatmentTimeline from "../../Basics/TreatmentTimeline";
-import ReportingHistory from "./ReportingHistory";
-import SectionLabel from "../../Components/SectionLabel";
-import PatientProfileDialogs from "./Dialogs/";
-import ArchivedOptions from "./Dialogs/ArchivedOptions";
-import ProfileHeader from "./Header";
-import useWindowSize from "../../Hooks/Resize";
-import MobileView from "./MobileView";
-import { useParams } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import useStores from '../../Basics/UseStores';
+import { observer } from 'mobx-react';
+import Styles from '../../Basics/Styles';
+import Colors from '../../Basics/Colors';
+import { useTranslation } from 'react-i18next';
+import PatientInfo from './Details/PatientInfo';
+import TreatmentStatus from './Adherence/AdherenceSummary';
+import SymptomSummary from './SymptomSummary';
+import TreatmentTimeline from '../../Basics/TreatmentTimeline';
+import ReportingHistory from './ReportingHistory';
+import SectionLabel from '../../Components/SectionLabel';
+import PatientProfileDialogs from './Dialogs/';
+import ArchivedOptions from './Dialogs/ArchivedOptions';
+import ProfileHeader from './Header';
+import useWindowSize from '../../Hooks/Resize';
+import MobileView from './MobileView';
+import { useParams } from 'react-router-dom';
+import AppointmentCard from './MobileView/AppointmentCard';
+import { Box } from '@material-ui/core';
 
 const Profile = observer(() => {
   const { id: patientId } = useParams();
 
   const { practitionerStore, patientProfileStore, uiStore } = useStores();
   const classes = useStyles();
-  const { t } = useTranslation("translation");
+  const { t } = useTranslation('translation');
 
   const isMobileView = useWindowSize().size.width < 830;
 
   const closeResetPassword = () => {
     patientProfileStore.closeResetPassword();
-    practitionerStore.newActivationCode = "";
+    practitionerStore.newActivationCode = '';
   };
 
   useEffect(() => {
@@ -43,7 +45,7 @@ const Profile = observer(() => {
 
   useEffect(() => {
     if (patientProfileStore.changes.success) {
-      uiStore.setAlert(t("coordinator.patientProfile.editDetails.success"));
+      uiStore.setAlert(t('coordinator.patientProfile.editDetails.success'));
     }
   }, [patientProfileStore.changes.success]);
 
@@ -51,7 +53,7 @@ const Profile = observer(() => {
   if (patientProfileStore.selectedPatient.accessError)
     return (
       <p className={classes.message}>
-        {t("coordinator.patientProfile.accessError")}
+        {t('coordinator.patientProfile.accessError')}
       </p>
     );
 
@@ -66,7 +68,7 @@ const Profile = observer(() => {
 const DesktopProfile = observer(() => {
   const { patientProfileStore } = useStores();
   const classes = useStyles();
-  const { t } = useTranslation("translation");
+  const { t } = useTranslation('translation');
 
   return (
     <div className={classes.patientContainer}>
@@ -77,10 +79,15 @@ const DesktopProfile = observer(() => {
         <TreatmentStatus />
         <SymptomSummary />
       </div>
+      <Box maxWidth="50%" style={{ ...Styles.profileCard }}>
+        <AppointmentCard
+          patient={patientProfileStore.selectedPatient.details}
+        />
+      </Box>
       <div className={classes.bottom}>
         <ReportingHistory />
         <div className={classes.treatmentTimeline}>
-          <SectionLabel>{t("timeline.title")}</SectionLabel>
+          <SectionLabel>{t('timeline.title')}</SectionLabel>
           <TreatmentTimeline
             weeksInTreatment={
               patientProfileStore.selectedPatient.details.weeksInTreatment
@@ -94,58 +101,58 @@ const DesktopProfile = observer(() => {
 
 const Loading = () => {
   const classes = useStyles();
-  const { t } = useTranslation("translation");
+  const { t } = useTranslation('translation');
   return (
     <div className={classes.message}>
-      <h1> {t("commonWords.loading")}...</h1>
+      <h1> {t('commonWords.loading')}...</h1>
     </div>
   );
 };
 
 const useStyles = makeStyles({
   listItem: {
-    fontWeight: "medium",
-    textTransform: "capitalize",
+    fontWeight: 'medium',
+    textTransform: 'capitalize',
   },
   top: {
-    display: "flex",
-    flexWrap: "wrap",
+    display: 'flex',
+    flexWrap: 'wrap',
     flexShrink: 0,
-    "& > div": {
-      flex: "1 1 0",
-      margin: "1em 0",
-      marginRight: "1em",
+    '& > div': {
+      flex: '1 1 0',
+      margin: '1em 0',
+      marginRight: '1em',
       ...Styles.profileCard,
     },
-    "& > div:last-of-type": {
+    '& > div:last-of-type': {
       marginRight: 0,
     },
   },
   treatmentTimeline: {
     ...Styles.profileCard,
-    alignSelf: "flex-start",
-    backgroundColor: "white",
-    minWidth: "280px",
-    padding: "1em",
-    marginLeft: "1em",
+    alignSelf: 'flex-start',
+    backgroundColor: 'white',
+    minWidth: '280px',
+    padding: '1em',
+    marginLeft: '1em',
   },
 
   patientContainer: {
-    height: "100vh",
+    height: '100vh',
     backgroundColor: Colors.lighterGray,
-    overflowY: "scroll",
-    width: "100%",
-    padding: "1em",
-    boxSizing: "border-box",
+    overflowY: 'scroll',
+    width: '100%',
+    padding: '1em',
+    boxSizing: 'border-box',
   },
   bottom: {
-    width: "100%",
-    display: "flex",
-    overflow: "hidden",
+    width: '100%',
+    display: 'flex',
+    overflow: 'hidden',
   },
   message: {
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
     ...Styles.flexCenter,
   },
 });
