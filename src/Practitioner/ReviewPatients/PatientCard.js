94f982e6df0310a4eb6061aa78777628d7e5d8fd
johnny2smooth <johnbp13@gmail.com>
2022-10-05 08:07:46 -0700

    last report days ago count added to patient card

--- src/Practitioner/ReviewPatients/PatientCard.js
+++ src/Practitioner/ReviewPatients/PatientCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import {
   makeStyles,
   Grid,
@@ -8,71 +8,71 @@ import {
   Collapse,
   CircularProgress,
   Button,
-} from "@material-ui/core";
+} from '@material-ui/core';
 import {
   Check,
   Message,
   ArrowDropDownCircle as Down,
   CheckCircle,
-} from "@material-ui/icons";
-import { Link } from "react-router-dom";
-import Colors from "../../Basics/Colors";
-import IssueArea from "./IssueArea";
-import AdherenceLabel from "./AdherenceLabel";
-import { useTranslation } from "react-i18next";
-import useAsync from "../../Hooks/useAsync";
-import PractitionerAPI from "../../API/PractitionerAPI";
-import TreatmentWeek from "./TreatmentWeek";
-import useToggle from "../../Hooks/useToggle";
-import IssueDetails from "./IssueDetails";
-import { DateTime } from "luxon";
+} from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+import Colors from '../../Basics/Colors';
+import IssueArea from './IssueArea';
+import AdherenceLabel from './AdherenceLabel';
+import { useTranslation } from 'react-i18next';
+import useAsync from '../../Hooks/useAsync';
+import PractitionerAPI from '../../API/PractitionerAPI';
+import TreatmentWeek from './TreatmentWeek';
+import useToggle from '../../Hooks/useToggle';
+import IssueDetails from './IssueDetails';
+import { DateTime } from 'luxon';
 
 const useStyles = makeStyles({
   container: {
-    width: "100%",
-    overflow: "wrap",
-    backgroundColor: "white",
-    boxSizing: "border-box",
-    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
-    "& a, & a:visited": {
-      textDecoration: "none",
+    width: '100%',
+    overflow: 'wrap',
+    backgroundColor: 'white',
+    boxSizing: 'border-box',
+    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.25)',
+    '& a, & a:visited': {
+      textDecoration: 'none',
     },
   },
   name: {
-    fontSize: "1.25em",
+    fontSize: '1.25em',
     color: Colors.buttonBlue,
   },
   bottomSection: {
-    width: "100%",
+    width: '100%',
   },
   messaged: {
-    fontSize: ".9em",
+    fontSize: '.9em',
     color: Colors.textDarkGray,
   },
   rotate: {
-    transform: "rotate(180deg)",
+    transform: 'rotate(180deg)',
   },
   reviewButton: {
     backgroundColor: Colors.calendarGreen,
-    padding: ".25em",
-    "&:disabled": {
+    padding: '.25em',
+    '&:disabled': {
       backgroundColor: Colors.lightgray,
     },
   },
   expand: {
-    width: "fit-content",
-    paddingRight: "0",
-    justifyContent: "flex-end",
-    textTransform: "none",
+    width: 'fit-content',
+    paddingRight: '0',
+    justifyContent: 'flex-end',
+    textTransform: 'none',
     color: Colors.buttonBlue,
   },
   reviewPhotoPrompt: {
-    lineHeight: "1.1em",
-    borderLeft: "solid 3px red",
-    paddingLeft: "5px",
+    lineHeight: '1.1em',
+    borderLeft: 'solid 3px red',
+    paddingLeft: '5px',
   },
   noIssues: {
-    fontStyle: "italic",
+    fontStyle: 'italic',
   },
 });
 
@@ -83,7 +83,7 @@ const PatientCard = ({
   isSimpleView,
 }) => {
   const classes = useStyles();
-  const { t } = useTranslation("translation");
+  const { t } = useTranslation('translation');
   const [reviewed, setReviewed] = useState(false);
   const [showDetails, toggleDetails] = useToggle(false);
 
@@ -92,7 +92,7 @@ const PatientCard = ({
   };
 
   const { execute, status, value, error } = useAsync(resolvePatient, false);
-  const success = status === "success";
+  const success = status === 'success';
 
   const handleExit = () => {
     if (success) {
@@ -123,7 +123,7 @@ const PatientCard = ({
             <Grid container>
               <CheckCircle style={{ color: Colors.green }} />
               <Box width=".5em" />
-              <Typography>{t("reviewIssues.submitted")}</Typography>
+              <Typography>{t('reviewIssues.submitted')}</Typography>
             </Grid>
           </Box>
         ) : (
@@ -140,10 +140,11 @@ const PatientCard = ({
                 <Box width=".5em" />
                 <AdherenceLabel patient={patient} />
               </Grid>
+              <LastContacted lastContacted={patient?.lastContacted} />
               <Box height=".5em" />
               {isReviewed && (
                 <Typography variant="body2">
-                  {t("reviewIssues.reviewedAt")}:{" "}
+                  {t('reviewIssues.reviewedAt')}:{' '}
                   {DateTime.fromISO(
                     patient.lastGeneralResolution
                   ).toLocaleString(DateTime.TIME_SIMPLE)}
@@ -166,12 +167,12 @@ const PatientCard = ({
                     />
                     <Box flexGrow={1} />
                     <Button className={classes.expand} onClick={toggleDetails}>
-                      <Typography style={{ paddingRight: ".5em" }} noWrap>
+                      <Typography style={{ paddingRight: '.5em' }} noWrap>
                         {showDetails
-                          ? t("messaging.moderation.hideUI")
-                          : t("reviewIssues.review")}
+                          ? t('messaging.moderation.hideUI')
+                          : t('reviewIssues.review')}
                       </Typography>
-                      <Down className={showDetails ? classes.rotate : ""} />
+                      <Down className={showDetails ? classes.rotate : ''} />
                     </Button>
                   </Grid>
                 ))}
@@ -183,7 +184,7 @@ const PatientCard = ({
                 )}
                 <ButtonArea
                   isReviewed={isReviewed}
-                  loading={status === "pending"}
+                  loading={status === 'pending'}
                   patient={patient}
                   resolvePatient={execute}
                 />
@@ -198,20 +199,20 @@ const PatientCard = ({
 
 const NoIssues = ({ resolvePatient, patient }) => {
   const classes = useStyles();
-  const { t } = useTranslation("translation");
+  const { t } = useTranslation('translation');
 
   return (
     <Grid alignItems="center" container wrap="nowrap">
       <Typography variant="body2" className={classes.noIssues}>
-        {t("reviewIssues.noIssues")}
+        {t('reviewIssues.noIssues')}
       </Typography>
       <Box flex="1" />
       <IconButton
         component={Link}
         to={`?onMessagingChannelId=${patient.channelId}`}
         style={{
-          backgroundColor: "rgba(66, 133, 244, 0.15)",
-          padding: ".25em",
+          backgroundColor: 'rgba(66, 133, 244, 0.15)',
+          padding: '.25em',
         }}
       >
         <Message style={{ color: Colors.buttonBlue }} />
@@ -231,7 +232,7 @@ const ButtonArea = ({
   isReviewed,
   isSimpleView,
 }) => {
-  const { t } = useTranslation("translation");
+  const { t } = useTranslation('translation');
   const disable = patient.unreviewedPhotos.length > 0;
   const classes = useStyles();
 
@@ -241,7 +242,7 @@ const ButtonArea = ({
         {disable && (
           <Box padding=".5em">
             <Typography className={classes.reviewPhotoPrompt}>
-              {t("reviewIssues.reviewBefore")}
+              {t('reviewIssues.reviewBefore')}
             </Typography>
           </Box>
         )}
@@ -249,8 +250,8 @@ const ButtonArea = ({
           component={Link}
           to={`?onMessagingChannelId=${patient.channelId}`}
           style={{
-            backgroundColor: "rgba(66, 133, 244, 0.15)",
-            padding: ".25em",
+            backgroundColor: 'rgba(66, 133, 244, 0.15)',
+            padding: '.25em',
           }}
         >
           <Message style={{ color: Colors.buttonBlue }} />
@@ -270,13 +271,34 @@ const ButtonArea = ({
                   variant="indeterminate"
                 />
               ) : (
-                <Check style={{ color: disable ? "white" : Colors.green }} />
+                <Check style={{ color: disable ? 'white' : Colors.green }} />
               )}
             </IconButton>
           </>
         )}
       </Grid>
     </Box>
+  );
+};
+
+const LastContacted = ({ lastContacted }) => {
+  const { t } = useTranslation('translation');
+
+  const daysAgo = !!lastContacted
+    ? Math.round(DateTime.fromISO(lastContacted).diffNow('days').days) * -1
+    : t('coordinator.tasksSidebar.noneYet');
+
+  console.log(lastContacted);
+
+  return (
+    <Grid wrap="nowrap" container>
+      <Typography>{t('coordinator.patientProfile.lastReport')}:</Typography>
+      <Box flexGrow={1} />
+      <Box width={'8px'} />
+      <Typography>
+        {daysAgo} {t('time.day_ago', { count: daysAgo })}
+      </Typography>
+    </Grid>
   );
 };
 
